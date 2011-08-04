d37adf97c33d25c8501a7f69334aed3826de6d24
Uldis Rudzitis <uldis.rudzitis@gmail.com>
2011-08-04 17:08:14 +0300

    default language EN in master branch, fixed HTML editor freeze on paste, allowing fb:comments and script tags in HTML editor for facebook comments

---
+++ engine/_lib/__tiny_mce/plugins/insertanything/langs/en_dlg.js
@@ -0,0 +1,6 @@
+tinyMCE.addI18n('en.insertanything_dlg',{
+title:"Insert / edit embedded media",
+general:"General",
+advanced:"Advanced",
+embed:'Paste here the <em>EMBED code</em> from youtube, vimeeo, google maps etc.'
+});
