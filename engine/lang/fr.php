edcd953cf7c3777d82f22239937a8ca7c4f08f9c
Raphaël Bastide <raphael.bastide@gmail.com>
2011-09-21 12:27:30 +0200

    French translation added, minor change in en.php

---
+++ engine/lang/fr.php
@@ -0,0 +1,40 @@
+<?php
+
+return array(
+
+	'berta_copyright_text' => 'Développé avec <a href="http://www.berta.lv/" target="_blank" title="Créez votre portfolio avec Berta en quelques minutes !">Berta</a>',
+
+	'margins_help_text' => 'Se servir des marges est un peu délicat. Utilisez les unités px ou em. Vous pouvez déclarer des marges de 4 façons, en écrivant : <br/><strong>1 valeur</strong> - déclarez la marge du haut, droite, bas et gauche en une seule valeur. Exemple : <em>10px</em>.<br /><strong>2 valeurs</strong> - déclarez la marge de haut et bas avec la première valeur, et gauche et droite avec la seconde valeur. Exemple : <em>0 5px</em>.<br /><strong>3 valeurs</strong> - déclarez la marge de haut avec la première valeur, de gauche et droite avec la seconde valeur et la marge de bas avec la troisième valeur. Exemple : <em>10px 0 20px</em>.<br /><strong>4 valeurs</strong> - déclarez toutes les marges selon l\'ordre suivant: haut, droit, bas, gauche. Exemple : <em>10px 0 20px 2px</em>.',
+	'units_help_text' => 'Les unités valides pour les valeurs numériques sont : <br /><strong>px</strong> - pixels<br /><strong>em</strong> - 1 em = une longueur de lettre M dans la fonte utilisée<br /><strong>%</strong> - pourcentage en fonction de la taille de la fonte ou de la dimension de l\'élément conteneur (ex. la page).',
+
+	'sections_help_text' => 'Les sections sont les principales divisions de votre site. Elles servent à contenir et organiser votre contenu. Chaque section est accessible dans le menu principal. ',
+	'external_link_help_text' => 'Si vous désirez qu\'un élément de votre menu principal pointe ailleurs que sur une section de votre site, utilisez le lien externe. Il peut être un lien de type email (ex., <em>mailto:exemple@site.net</em>) ou un lien vers un autre site web (ex. <em>http://www.example.com</em>).',
+
+	'password_help_text' => 'Le mot de passe doit comporter au moins 6 caractères alphanumériques (A-Z, a-z, 0-9).',
+	
+	'welcome_text__not_installed' => '<h2>Merci d\'avoir choisi Berta !</h2>
+		   <p>Berta n\'est pas encore installé.<br />Merci de vous <a href="./engine/">connecter</a> et de suivre la procédure d\'installation.</p>',
+
+	'welcome_text__not_supported' => '<h2>Merci d\'avoir choisi Berta !</h2>
+										<p>Ce serveur n\'est pas compatible avec Berta.<br />
+										Merci de vérifier que la version de PHP installée sur le serveur est égale ou supérieure à 5.</p>',
+	'setup_info_text' => 'Cliquez sur les champs sur fond jaune pour les éditer.
+							Puis, appuyez sur Entrée ou cliquez ailleurs pour enregistrer.
+							Chaque élément de votre site ayant un fond jaune est donc modifiable. Vous serez en mesure de changer ces paramètres plus tard.',
+
+	'googleFont_description' => 'Tapez le nom d\'une fonte Google font. Pour voir les fontes disponibles, rendez-vous sur <a href="http://www.google.com/webfonts" target="_blank">Google web fonts</a>. Attention  – les fontes Google font prennent le dessus sur les fontes système. Laissez le champ vide pour utiliser une fonte système. Exemple : <em>Marvel</em> ou <em>Marvel:700italic</em>',
+	'description_tagsMenu_x' => 'Position X du sous-menu en pixels (ex. 10px)',
+	'description_tagsMenu_y' => 'Position Y du sous-menu en pixels (ex. 10px)',
+	'description_menu_position' => 'Position du menu',
+	'description_banner' => 'Les bannières sont des images visibles sur toutes les sections. Vous pouvez les utiliser comme boutons ou icônes de réseaux sociaux sur votre site.',
+	'description_banner_link' => 'N\'oubliez pas \'http://\' devant les liens de bannière',
+	'description_language' => 'Choisissez votre language pour l\'interface de Berta. Rafraîchissez la page pour pour appliquer les modifications.',
+	'Heading position' => 'Position de l\'entête : fixe (Fixed) ou absolue (Absolute). La position fixe restera toujours en place sur la page, la positions absolue se déplace avec le contenu au défilement.',
+	'description_heading_position' => 'Position de la description d\'entête : fixe (Fixed) ou absolue (Absolute). La position fixe restera toujours en place sur la page, la positions absolue se déplace avec le contenu au défilement.',
+	'description_submenu_alwaysopen' => 'Un sous-menu est ouvert lorsqu\'un élément de menu est actif.',
+	'' => '',
+
+
+)
+
+?>
