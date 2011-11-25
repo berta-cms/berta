4bd7f3e77d604f23dd300199018899878bd385d5
Uldis Rudzitis <uldis.rudzitis@gmail.com>
2011-11-25 14:00:22 +0200

    shop updates

--- templates/messy-0.3.7/plugins/function.customEntryHeader.php
+++ templates/messy-0.3.7/plugins/function.customEntryHeader.php
@@ -15,14 +15,16 @@ function smarty_function_customEntryHeader($params, &$smarty) {
 	if($berta->environment != 'engine') return '';
 	
 	$markedValue = !empty($params['entry']['marked']['value']) ? 1 : 0;
-	$fixedValue = !empty($params['entry']['fixed']['value']) ? 1 : 0;
-	
 	$tags=isset($params['entry']['tags'])?implode(', ',$params['entry']['tags']):'';
-	$customWidth=isset($params['entry']['width']['value']) ? $params['entry']['width'] : '';
+
+	$shopMenuEntry = null;
+	if(isset($params['ishopentry']) && $params['ishopentry'] == 1) {
+		//build shop menu entry...
+		$shopMenuEntry .= '<div class="xEntrySeperator"></div><div class="xEntryBoxParams"><b>Attribute</b><div class="xEditable xProperty-cartAttributes xCaption-attribute cCartAttributes">'.$params['entry']['cartAttributes'].'</div></div>';
+	}
 	
 	return <<<DOC
 		<a class="xCreateNewEntry xPanel xAction-entryCreateNew" href="#"><span>create new entry here</span></a>
-	
 		<div class="xEntryEditWrap">
 			<div class="xEntryEditWrapButtons xPanel">
 				
@@ -41,20 +43,13 @@ function smarty_function_customEntryHeader($params, &$smarty) {
 						<a href="#" class="xEntryToBack" title="send to back behind others"><span>Send to back</span></a>
 					</li>				
 					<li>
-						<a><div class="xEntryCheck"><label><span class="xEditableRealCheck xProperty-fixed">$fixedValue</span>Fixed position</label></div></a>
-					</li>						
-					<li>
-						<div class="customWidth">
-							<div title="$customWidth" class="xEditableRC xProperty-width">$customWidth</div>
-						</div>	
-					</li>	
-					<li>
 						<a><div class="xEntryCheck"><label><span class="xEditableRealCheck xProperty-marked">$markedValue</span>Marked</label></div></a>
 					</li>
 					<li>					
 						<a href="#" class="xEntryDelete xAction-entryDelete" title="delete"><span>Delete</span></a>
 					</li>									
 				</ul>
+	{$shopMenuEntry}
 			</div>			
 DOC;
 }
