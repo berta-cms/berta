e9f1fb8b6f30ad46069ad01b95b0ea554a7370a0
Kaspars Zarinovs <k.zarinovs@gmail.com>
2012-04-03 23:28:45 +0300

    Added additional conditionals for version check & update.

--- engine/inc.version_check_and_updates.php
+++ engine/inc.version_check_and_updates.php
@@ -5,34 +5,35 @@ include_once($ENGINE_ROOT . '_classes/class.bertaeditor.php');
 if( !empty($berta->content['entry']) ) {
     $tplName = $berta->template->settings->templateName;
 
-    switch($tplName) {
-        case 'messy':
-            foreach( $berta->content['entry'] as $eKey => $e ) {
-                $galleryType = $e['mediaCacheData']['@attributes']['type'];
+    // Updates for 0.8.2
+    if( empty($berta->content['@attributes']['last_upd_ver']) || ($berta->content['@attributes']['last_upd_ver'] < 1082) ) {
+        switch($tplName) {
+            case 'messy':
+                foreach( $berta->content['entry'] as $eKey => $e ) {
+                    $galleryType = $e['mediaCacheData']['@attributes']['type'];
 
-                if( $galleryType == 'row' ) {
-                    $berta->content['entry'][$eKey]['mediaCacheData']['@attributes']['type'] = 'pile';
+                    if( $galleryType == 'row' ) {
+                        $berta->content['entry'][$eKey]['mediaCacheData']['@attributes']['type'] = 'pile';
+                    }
                 }
-            }
-            break;
+                break;
 
-        case 'mashup':
-        case 'white':
-            foreach( $berta->content['entry'] as $eKey => $e ) {
-                $galleryType = $e['mediaCacheData']['@attributes']['type'];
+            case 'mashup':
+            case 'white':
+                foreach( $berta->content['entry'] as $eKey => $e ) {
+                    $galleryType = $e['mediaCacheData']['@attributes']['type'];
 
-                if( $galleryType == 'row' ) {
-                    $berta->content['entry'][$eKey]['mediaCacheData']['@attributes']['type'] = 'column';
+                    if( $galleryType == 'row' ) {
+                        $berta->content['entry'][$eKey]['mediaCacheData']['@attributes']['type'] = 'column';
+                    }
                 }
-            }
-            break;
+                break;
+        }
+
+        $berta->content['@attributes']['last_upd_ver'] = $options['int_version'];
+        BertaEditor::saveBlog($berta->sectionName, $berta->content);
+        //header('Location: ' . $_SERVER['PHP_SELF']);
     }
 }
-
-$berta->content['@attributes']['last_upd_ver'] = $options['int_version'];
-
-BertaEditor::saveBlog($berta->sectionName, $berta->content);
-
-//header('Location: ' . $_SERVER['PHP_SELF']);
 
 ?>
