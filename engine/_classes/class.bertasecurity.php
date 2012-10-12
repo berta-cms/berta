e5ad8dd80b737963bfe2f8d6331a9ee206559068
Uldis Rudzitis <uldis.rudzitis@gmail.com>
2012-10-12 18:09:34 +0300

    bug fix for hosted sites

--- engine/_classes/class.bertasecurity.php
+++ engine/_classes/class.bertasecurity.php
@@ -63,7 +63,7 @@ class BertaSecurity {
 
 	public function isLoggedIn($url, $key) {
 		$ch = curl_init();
-		$timeout = 5;
+		$timeout = 30;
 		curl_setopt($ch,CURLOPT_URL,$url);
 		curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
 		curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
