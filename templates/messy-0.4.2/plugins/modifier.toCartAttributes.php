2c41c053efa990aaa9c4d7f0d37449cc0aa195d8
Uldis Rudzitis <uldis.rudzitis@gmail.com>
2012-12-18 16:12:43 +0200

    fix for shop select box - trim variables

--- templates/messy-0.4.2/plugins/modifier.toCartAttributes.php
+++ templates/messy-0.4.2/plugins/modifier.toCartAttributes.php
@@ -10,17 +10,18 @@
  */
 function smarty_modifier_toCartAttributes($attr) {
 	global $berta;
-	
+
 	if(strlen($attr)>0) {
 		$attr = explode(',', $attr);
 		$attrOutStr = '<select class="cart_attributes">';
 		foreach($attr as $aout) {
+			$aout = trim($aout);
 			$attrOutStr .= '<option value="'.htmlspecialchars($aout).'">'.htmlspecialchars($aout).'</option>';
 		}
 		$attrOutStr .= '</select>';
 		return $attrOutStr;
 	}
-	
+
 	return '';
 }
 ?>
