4d86edb2589900c54484e6f4f78e1a51cf2129d7
Uldis Rudzitis <uldis.rudzitis@gmail.com>
2013-02-07 17:11:03 +0200

    added weight attribute for shop products

--- templates/messy-0.4.2/plugins/function.customEntryHeader.php
+++ templates/messy-0.4.2/plugins/function.customEntryHeader.php
@@ -5,15 +5,15 @@
  * File:     function.customEntryHeader.php
  * Type:     function
  * Name:     customEntryHeader
- * Purpose:  
+ * Purpose:
  * -------------------------------------------------------------
  */
 function smarty_function_customEntryHeader($params, &$smarty) {
 	global $berta;
 	$settings = $berta->template->settings;
-	
+
 	if($berta->environment != 'engine') return '';
-	
+
 	$markedValue = !empty($params['entry']['marked']['value']) ? 1 : 0;
 
 	$fixedValue = !empty($params['entry']['fixed']['value']) ? 1 : 0;
@@ -22,46 +22,53 @@ function smarty_function_customEntryHeader($params, &$smarty) {
 
 	$shopMenuEntry = null;
 	if(isset($params['ishopentry']) && $params['ishopentry'] == 1) {
+
+		$xUnits = $settings->get('shop', 'weightUnit');
+
 		//build shop menu entry...
-		$shopMenuEntry .= '<div class="xEntrySeperator"></div><div class="xEntryBoxParams"><b>Attribute</b><div class="xEditable xProperty-cartAttributes xCaption-attribute cCartAttributes">'.$params['entry']['cartAttributes'].'</div></div>';
+		$shopMenuEntry .= '	<div class="xEntrySeperator"></div>
+							<div class="xEntryBoxParams"><b>Attribute</b>
+								<div class="xEditable xProperty-cartAttributes xCaption-attribute cCartAttributes">'.$params['entry']['cartAttributes'].'</div>
+								<div class="xEditable xProperty-weight xCaption-weight xUnits-'.$xUnits.'">'.$params['entry']['weight'].'</div>
+							</div>';
 	}
-	
+
 	return <<<DOC
 		<a class="xCreateNewEntry xPanel xAction-entryCreateNew" href="#"><span>create new entry here</span></a>
 		<div class="xEntryEditWrap">
 			<div class="xEntryEditWrapButtons xPanel">
-				
+
 				<a href="#" class="xEntryMove xHandle" title="Drag + Shift to move all"><span>move entry</span></a>
-								
+
 				<div class="tagsList">
 					<div title="$tags" class="xEditableRC xProperty-submenu xFormatModifier-toTags">$tags</div>
-				</div>				
-				
+				</div>
+
 				<div class="xEntryDropdown"></div>
-				
+
 			</div>
 			<div class="xEntryDropdownBox">
 				<ul>
-					<li>				
+					<li>
 						<a href="#" class="xEntryToBack" title="send to back behind others"><span>Send to back</span></a>
-					</li>		
+					</li>
  					<li>
 						<a><div class="xEntryCheck"><label><span class="xEditableRealCheck xProperty-fixed">$fixedValue</span>Fixed position</label></div></a>
-					</li>						
+					</li>
 					<li>
 						<div class="customWidth">
 							<div title="$customWidth" class="xEditableRC xCSSUnits-1 xProperty-width">$customWidth</div>
-						</div>	
-					</li>								
+						</div>
+					</li>
 					<li>
 						<a><div class="xEntryCheck"><label><span class="xEditableRealCheck xProperty-marked">$markedValue</span>Marked</label></div></a>
 					</li>
-					<li>					
+					<li>
 						<a href="#" class="xEntryDelete xAction-entryDelete" title="delete"><span>Delete</span></a>
-					</li>									
+					</li>
 				</ul>
 				{$shopMenuEntry}
-			</div>			
+			</div>
 DOC;
 }
 ?>
