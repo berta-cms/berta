1cfffc09735a658c1d1ea8f024569bc918c0eb23
Uldis Rudzitis <uldis.rudzitis@gmail.com>
2013-02-20 15:25:50 +0200

    google fonts subsets added to support additional chars for ru, lv, gr languages

--- templates/_plugins/function.googleWebFontsAPI.php
+++ templates/_plugins/function.googleWebFontsAPI.php
@@ -5,7 +5,7 @@
  * File:     function.bertaAbsPosStyle.php
  * Type:     function
  * Name:     bertaAbsPosStyle
- * Purpose:  
+ * Purpose:
  * -------------------------------------------------------------
  */
 function smarty_function_googleWebFontsAPI() {
@@ -14,26 +14,26 @@ function smarty_function_googleWebFontsAPI() {
 	$s = $berta->template->settings;
 	$fonts=array();
 	$js_include='';
-	
+
 	$generalFontSettings = $s->get('generalFontSettings', 'googleFont');
 	if (!empty($generalFontSettings)) {
 		$fonts[]=urlencode($generalFontSettings);
 	}
-	
+
 	$pageHeading = $s->get('pageHeading', 'googleFont');
 	if (!empty($pageHeading)) {
 		$fonts[]=urlencode($pageHeading);
-	}	
-		
+	}
+
 	$menu = $s->get('menu', 'googleFont');
 	if (!empty($menu)) {
 		$fonts[]=urlencode($menu);
-	}	
-	
+	}
+
 	$subMenu = $s->get('subMenu', 'googleFont');
 	if (!empty($subMenu)) {
 		$fonts[]=urlencode($subMenu);
-	}	
+	}
 
 	$entryHeading = $s->get('entryHeading', 'googleFont');
 	if (!empty($entryHeading)) {
@@ -44,33 +44,31 @@ function smarty_function_googleWebFontsAPI() {
 	if (!empty($entryFooter)) {
 		$fonts[]=urlencode($entryFooter);
 	}
-	
+
 	$sideBar = $s->get('sideBar', 'googleFont');
 	if (!empty($sideBar)) {
 		$fonts[]=urlencode($sideBar);
 	}
-	
+
 	$tagsMenu = $s->get('tagsMenu', 'googleFont');
 	if (!empty($sideBar)) {
 		$fonts[]=urlencode($tagsMenu);
-	}	
-	
+	}
+
 	$heading = $s->get('heading', 'googleFont');
 	if (!empty($heading)) {
 		$fonts[]=urlencode($heading);
-	}		
+	}
 
 	$heading = $berta->settings->get('shop', 'priceItemgoogleFont');
 	if (!empty($heading)) {
 		$fonts[]=urlencode($heading);
-	}	
+	}
 
 	if ($fonts){
-		$js_include="<link href='http://fonts.googleapis.com/css?family=".implode('|',$fonts)."' rel='stylesheet' type='text/css'>";
+		$js_include="<link href='http://fonts.googleapis.com/css?family=".implode('|',$fonts)."&amp;subset=latin,latin-ext,cyrillic-ext,greek-ext,greek,vietnamese,cyrillic' rel='stylesheet' type='text/css'>";
 	}
-	
-	//print_R ($generalFontSettings);	
-	
+
 	return $js_include;
 }
 
