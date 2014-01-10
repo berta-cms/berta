113591d8b84386a55834c3b5579daacc28b4da89
Uldis Rudzitis <uldis.rudzitis@gmail.com>
2014-01-10 11:51:46 +0200

    embed iframe fix for responsive default template

---
+++ templates/default/default.js
@@ -0,0 +1,36 @@
+var DefaultTemplate = new Class({
+
+    isResponsive: false,
+
+	initialize: function() {
+		window.addEvent('domready', this.onDOMReady.bind(this));
+	},
+
+	onDOMReady: function() {
+        this.isResponsive = $$('.xResponsive').length;
+
+        if (this.isResponsive) {
+            if (bertaGlobalOptions.environment == 'site'){
+                this.iframeResponsiveFix($$('iframe'));
+            }
+        }
+    },
+
+    iframeResponsiveFix: function(el) {
+        el.each(function(item) {
+            var width = item.get('width');
+            var height = item.get('height');
+            var wrapper = new Element('div', {'class': 'iframeWrapper'});
+
+            if (width && height){
+                wrapper.setStyle('padding-bottom', height*100/width + '%');
+            }
+
+            if ( !item.getParent().hasClass('iframeWrapper') ) { //if no iframeWrapper already exists
+                wrapper.wraps(item);
+            }
+        });
+    }
+});
+
+var defaultTemplate = new DefaultTemplate();
