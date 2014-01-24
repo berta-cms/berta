fe604981fa1dc49aa66f1d6c44d0111af5e4c859
gabrielbr <ogabriel@gmail.com>
2014-01-24 16:32:39 -0200

    All the brazilian portuguese is done
    All the translation is done.
    Any bug, let me know

--- engine/lang/pt-br.php
+++ engine/lang/pt-br.php
@@ -4,39 +4,38 @@ return array(
 
 	'berta_copyright_text' => 'Desenvolvido com <a href="http://www.berta.me/" target="_blank" title="Crie seu própio website com Berta em alguns minutos!">Berta</a>',
 
-	'margins_help_text' => 'O parâmetro Margin é traiçoeiro. Use px ou em como unidades de medida. Você pode definir os parâmetros Margin de 4 maneiras diferentes:<br/><strong>1 Valor</strong> - define que as margens do topo, direita, inferior e esquerda com os mesmos valores. Exemplo:<em>10px</em>.<br /><strong>2 valores</strong>
-
- Example: <em>10px</em>.<br /><strong>2 values</strong> - sets top and bottom margins to the first value, left and right - to the second. Example: <em>0 5px</em>.<br /><strong>3 values</strong> - sets top margin to the first value, left and right - to the second, bottom - to the third value. Example: <em>10px 0 20px</em>.<br /><strong>4 values</strong> - sets all margins in the following order: top, right, bottom, left. Example: <em>10px 0 20px 2px</em>.',
-	'units_help_text' => 'Valid units for any numerical value are:<br /><strong>px</strong> - pixels. Example: <em>10px</em>. By using just numbers value WILL NOT change.<br /><strong>em</strong> - 1em = one length of letter M in the font used. Example: <em>1em</em><br /><strong>%</strong> - percent of the font size or percent of the dimensions of the container element (e.g. the page etc.)',
-
-	'sections_help_text' => 'Sections are main divisions in your site. Think of them as containers for your content. They appear as menu items in the main menu. ',
-	'external_link_help_text' => 'If you want any of the items in your main menu to lead the visitor somewhere else than your site, specify the external link. It can be an email link (e.g., <em>mailto:sombeody@someplace.net</em>) or a link to another website (e.g. <em>http://www.example.com</em>).',
-
-	'password_help_text' => 'Password must be at least 6 characters long<br /> and containing alphanumeric (A-Z, a-z, 0-9) characters.',
+	'margins_help_text' => 'O parâmetro Margin é traiçoeiro. Use px ou em como unidades de medida. Você pode definir os parâmetros Margin de 4 maneiras diferentes:<br/><strong>1 Valor</strong> - define que as margens do topo, direita, inferior e esquerda com os mesmos valores. Exemplo:<em>10px</em>.<br /><strong>2 valores</strong> - define, com o primeiro valor, as margens do topo e inferior e, com o segundo valor, esquerda e direita. Exemplo:<em>0 5px</em>.<br /><strong> 3 valores </strong> - define a margem do topo para o primeiro valor, esquerda e direita para o segundo valor, inferior para o terceiro valor. Exemplo: <em>10px 0 20px</em>.<br /><strong>4 valores</strong> - define os valores de margem na seguinte ordem: topo, direita, inferior e esquerda. Exemplo: <em>10px 0 20px 2px</em>.',
 	
-	'welcome_text__not_installed' => '<h2>Thank you for choosing Berta!</h2>
-		   <p>Berta is not set up.<br />Please <a href="./engine/">log in</a> and follow the set up procedure.</p>',
+'units_help_text' => 'Unidades de medida validas para qualquer valor numérico são:<br /><strong>px</strong> - pixels. Exemplo: <em>10px</em>. Usando apenas números NÃO FUNCIONARÁ.<br /><strong>em</strong> - 1em = um comprimento da letra M da font utilizada. Example: <em>1em</em><br /><strong>%</strong> - porcentagem do tamanho da fonte ou a porcentagem das dimensões do elemento container (ex. a página, uma div, etc.)',
 
-	'welcome_text__not_supported' => '<h2>Thank you for choosing Berta!</h2>
-										<p>This server does not meet Berta\'s requirements.<br />
-										Please check that PHP version 5 or above is installed on the server.</p>',
-	'setup_info_text' => 'Click on the fields with yellow background to edit them.
-							Then press Enter or click anywhere outside the field to save.
-							This way it will be throughout your site — all that has a yellow background is editable. You will also be able to change these settings later.',
+	'sections_help_text' => 'Sessões são as principais divisões do seu site. Pense nele como containers para seus conteudos. Eles são exibidos como itens no menu principal. ',
+	'external_link_help_text' => 'Se você deseja que qualquer um do seus itens do menu principal seja um link para um lugar qualquer externo ao seu site, especifique este link externo. Ele pode ser um link para email (ex. <em>mailto:alguem@deumsite.com.br</em>) ou o link para um outro site qualquer (ex. <em>http://www.exemplo.com.br</em>).',
 
-	'googleFont_description' => 'Type in any google font name. To check available fonts go to <a href="http://www.google.com/webfonts" target="_blank">Google web fonts</a>. Remember – google font overrides system font. Leave empty if you want to use system font. Example: <em>Marvel</em>, <em>Marvel:700italic</em> or <em>Josefin Slab:700italic</em>',
-	'description_tagsMenu_x' => 'Submenu X position in pixels (i.e. 10px)',
-	'description_tagsMenu_y' => 'Submenu Y position in pixels (i.e. 10px)',
-	'description_menu_position' => 'Menu position',
-	'description_banner' => 'Banners are images which are visible in all sections. Use it for buttons or social icons in your site',
-	'description_banner_link' => 'Banner link \'http://\' should be in front of the address',
-	'description_language' => 'Choose language of berta interface. Refresh site, to apply.',
-	'Heading position' => 'Heading position fixed or absolute. Fixed always stays in one place, absolute moves together with content.',
-	'description_heading_position' => 'Heading position fixed or absolute. Fixed always stays in one place, absolute moves together with content.',
-	'description_submenu_alwaysopen' => 'Submenu is open when menu item is current.',
-	'mobile_device_detected' => 'You can\'t use mobile device to edit your site!',
-	'javascript_include' => 'Javascript code included right before closing &lt;/body&gt; element. Learn more how to <a href="https://github.com/berta-cms/berta/wiki/Include-JavaScript-code" target="_blank" title="How to include JavaScript code">include JavaScript code</a>.',
-	'description_custom_css' => 'Place your custom CSS code here. Any existing CSS definitions will be overwritten. For more information visit our <a href="https://github.com/berta-cms/berta/wiki/Add-custom-CSS" target="_blank">WIKI</a>.',
+	'password_help_text' => 'O Password deve ter pelo menos 6 caracteres <br /> e ser alfanumérico (A-Z, a-z, 0-9).',
+	
+	'welcome_text__not_installed' => '<h2>Obrigado por escolher Berta!</h2>
+		   <p>Berta não está instalado.<br />Por Favor faça<a href="./engine/">log in</a> e siga as instruções de instalação.</p>',
+
+	'welcome_text__not_supported' => '<h2>Obrigado por escolher Berta!</h2>
+										<p>Este servidor não atende aos requisitos do Berta.<br />
+										Por favor, verifique se possui a versão 5 ou superior do PHP instalado em seu servidor.</p>',
+	'setup_info_text' => 'Clique nos campos em amarelo para edita-los.
+							Então aperte Enter ou clique em qualquer lugar fora do campo para salvar.
+							Desta maneira funcionará seu site — tudo que for marcado de amarelo é editável. Você tambem poderá mudar estas configurações depois.',
+
+	'googleFont_description' => 'Digite qualquer nome de uma fonte Google. Para verificar as fontes disponíveis vá para <a href="http://www.google.com/webfonts" target="_blank">Google web fonts</a>. Lembre-se – fonts do google sobrescrevem as fontes de sistema. Deixe o campo em branco se desejar manter fontes de sistema. Examplo: <em>Marvel</em>, <em>Marvel:700italic</em> or <em>Josefin Slab:700italic</em>',
+	'description_tagsMenu_x' => 'Posição X do Submenu em pixels (ex. 10px)',
+	'description_tagsMenu_y' => 'Posição Y do Submenu em pixels (ex. 10px)',
+	'description_menu_position' => 'Posição do Menu',
+	'description_banner' => 'Banners são imagens que são visíveis em todas as sessões. Use-as como butões ou icones de redes sociais em seu site.',
+	'description_banner_link' => 'Link do Banner \'http://\' deve estar à frente do endereço.',
+	'description_language' => 'Escolha o idioma da interface do Berta. Atualize o site para aplicar.',
+	'Heading position' => 'Posição do cabeçalho: fixed (fixa) ou absolute (absoluta). Posição fixa significa que o cabeçalho sempre fica no mesmo local, posição absoluta o cabeçalho se move junto com o conteudo.',
+	'description_heading_position' => 'Posição do cabeçalho: fixed (fixa) ou absolute (absoluta). Posição fixa significa que o cabeçalho sempre fica no mesmo local, posição absoluta o cabeçalho se move junto com o conteudo.',
+	'description_submenu_alwaysopen' => 'O Submenu permanece aberto quando o item do Menu é o selecionado.',
+	'mobile_device_detected' => 'Você não pode usar um dispostivo móvel para editar seu site!',
+	'javascript_include' => 'Código Javascript incluido logo antes do fechamento do elemento &lt;/body&gt. Aprenda mais em como <a href="https://github.com/berta-cms/berta/wiki/Include-JavaScript-code" target="_blank" title="Como Incluir código Javascript">incluir código Javascript</a>(em inglês).',
+	'description_custom_css' => 'Coloque seu CSS customizado aqui. Qualquer definiçãod e CSS existente será sobrescrita. Para mais informações visite o nosso <a href="https://github.com/berta-cms/berta/wiki/Add-custom-CSS" target="_blank">WIKI</a>(em inglês).',
 )
 
 ?>
