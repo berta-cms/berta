88fb1e3e093294c7259f6df2da3ff7a7f23f6036
Andis Roze <andis.roze.438@gmail.com>
2016-02-14 15:21:37 +0200

    1. add domReady function. 2. Fix bugs in template loading.

--- engine/js/ng/index.js
+++ engine/js/ng/index.js
@@ -3,6 +3,7 @@
 
   var createStoreWithMiddleware = Redux.applyMiddleware(redux_middleware)(Redux.createStore);
   var site = getCurrentSite();
+  domReady(Templates.load);
 
   window.redux_store = createStoreWithMiddleware(root_reducer);
   redux_store.dispatch(Actions.getState(site));
--- engine/js/ng/utils.js
+++ engine/js/ng/utils.js
@@ -20,31 +20,89 @@
     return q.site === undefined ?  '0' : q.site;
   };
 
-  window.Templates = {
-    templates: {},
+  var Templates = {
+        templates: {}
+      };
 
-    loadTemplates: function() {
-      var templateList = document.getElementById('templateList');
+  Templates.load = function load() {
+    var self = this,
+        templateList = document.getElementById('templateList');
 
-      if (templateList) {
-        var templates = templateList.querySelectorAll('script');
-        templates.forEach(function(template, index){
-          if(template.getAttribute('type') === 'text/template'){
-            this.templates[template.getAttribute('id')] = template.innerHtml;
-          }
-        }.bind(this));
-      }
-    },
+    if (templateList) {
+      var templates = Array.prototype.slice.call(
+            templateList.querySelectorAll('script'),
+            0
+          );
 
-    // Get template by name from hash of preloaded templates
-    get: function(name) {
-      return this.templates[name];
+      templates.forEach(function(template, index){
+        if(template.getAttribute('type') === 'text/template'){
+          this.templates[template.getAttribute('id')] = template.innerHTML;
+        }
+      }.bind(this));
     }
-  };
+  }.bind(Templates);
+
+  Templates.get = function get(name) {
+    return this.templates[name];
+  }.bind(Templates);
+
+  window.Templates = Templates;
 
   String.prototype.replaceTokens = function replaceTokens(replacement) {
     return this.replace(/<\%=([^%>]+)\%>/g, function(str, match) {
       return replacement[match];
     });
-  }
+  };
+
+  window.domReady = function(callback) {
+    var ready = false,
+        detach = function() {
+          if(document.addEventListener) {
+            document.removeEventListener("DOMContentLoaded", completed);
+            window.removeEventListener("load", completed);
+          } else {
+            document.detachEvent("onreadystatechange", completed);
+            window.detachEvent("onload", completed);
+          }
+        },
+        completed = function() {
+          if(!ready && (document.addEventListener || event.type === "load" || document.readyState === "complete")) {
+            ready = true;
+            detach();
+            callback();
+          }
+        };
+
+    if(document.readyState === "complete") {
+      callback();
+    } else if(document.addEventListener) {
+      document.addEventListener("DOMContentLoaded", completed);
+      window.addEventListener("load", completed);
+    } else {
+      document.attachEvent("onreadystatechange", completed);
+      window.attachEvent("onload", completed);
+
+      var top = false;
+
+      try {
+        top = window.frameElement === null && document.documentElement;
+      } catch(e) {}
+
+      if(top && top.doScroll) {
+        (function scrollCheck() {
+          if(ready) return;
+
+          try {
+              top.doScroll("left");
+          } catch(e) {
+              return setTimeout(scrollCheck, 50);
+          }
+
+          ready = true;
+          detach();
+          callback();
+        })();
+      }
+    }
+  };
 })(window, document);
