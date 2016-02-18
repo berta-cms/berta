6f6a341c8f899f585c7f317b124e07611f8a3556
Andis Roze <andis.roze.438@gmail.com>
2016-02-18 16:28:57 +0200

    Copy editable class names from the backend

--- engine/js/ng/constants/constants.js
+++ engine/js/ng/constants/constants.js
@@ -2,4 +2,21 @@
   'use strict';
 
   window.API_ROOT = '/_api/v1/';
+
+  window.editables = {
+    xEditable: 'xEditable',                     // simple input
+    xEditableColor: 'xEditableColor',           // simple input
+    xEditableSelect: 'xEditableSelect',         // select
+    xEditableSelectRC: 'xEditableSelectRC',     // select
+    xEditableFontSelect: 'xEditableFontSelect', // select
+    xEditableImage: 'xEditableImage',           // image upload
+    xEditableICO: 'xEditableICO',               // ico upload
+    xEditableTA: 'xEditableTA',                 // textarea
+    xEditableMCE: 'xEditableMCE',               // textarea
+    xEditableMCESimple: 'xEditableMCESimple',   // textarea
+    xEditableRC: 'xEditableRC',                 // with "real" content
+    xEditableYesNo: 'xEditableYesNo',           // the "yes/no" switch
+    xEmpty: 'xEmpty'
+  };
+
 })(window, document);
