b63bfa04c770b62ae699c1d9427acacbd63048ac
Uldis Rudzitis <uldis.rudzitis@gmail.com>
2018-05-23 12:55:26 +0300

    Get styles for entry

--- _api_app/app/Sites/Sections/Entries/SectionEntryRenderService.php
+++ _api_app/app/Sites/Sections/Entries/SectionEntryRenderService.php
@@ -36,10 +36,24 @@ class SectionEntryRenderService
     {
         $entry = $this->entry;
 
-        $entry['classList'] = $this->getClassList();
         $entry['entryId'] = $this->getEntryId();
+        $entry['classList'] = $this->getClassList();
+        $entry['styleList'] = $this->getStyleList();
 
         return $entry;
+    }
+
+    private function getEntryId() {
+        $section_type = isset($this->section['@attributes']['type']) ? $this->section['@attributes']['type'] : null;
+
+        if ($section_type == 'portfolio' && isset($this->entry['content']['title']) && $this->entry['content']['title']) {
+            $title = $this->entry['content']['title'];
+        } else {
+            $title = 'entry-'.$this->entry['id'];
+        }
+        $slug = Helpers::slugify($title, '-', '-');
+
+        return $slug;
     }
 
     private function getClassList() {
@@ -76,17 +90,56 @@ class SectionEntryRenderService
         return implode(' ', $classes);
     }
 
-    private function getEntryId() {
+    private function getStyleList() {
+        $styles = [];
+        $template_name = explode('-', $this->site_settings['template']['template'])[0];
         $section_type = isset($this->section['@attributes']['type']) ? $this->section['@attributes']['type'] : null;
+        $isResponsive = isset($this->site_template_settings['pageLayout']['responsive']) ? $this->site_template_settings['pageLayout']['responsive'] : 'no';
 
-        if ($section_type == 'portfolio' && isset($this->entry['content']['title']) && $this->entry['content']['title']) {
-            $title = $this->entry['content']['title'];
-        } else {
-            $title = 'entry-'.$this->entry['id'];
+        if ($template_name == 'messy') {
+            if ($section_type == 'portfolio') {
+                $isResponsive = 'yes';
+            }
+
+            if ($isResponsive == 'yes') {
+                return null;
+            }
+
+            if (isset($this->entry['content']['positionXY'])) {
+                list($left, $top) = explode(',', $this->entry['content']['positionXY']);
+            } else {
+                // new (non updated) entries are placed in top right corder
+                $placeInFullScreen = isset($this->entry['updated']);
+                list($left, $top) = [
+                    rand($placeInFullScreen ? 0 : 900, 960),
+                    rand($placeInFullScreen ? 0 : 30, $placeInFullScreen ? 600 : 200)
+                ];
+            }
+
+            $styles[] = ['left' => $left . 'px'];
+            $styles[] = ['top' => $top . 'px'];
+
+            if (isset($this->entry['content']['width']) && $this->entry['content']['width']) {
+                $styles[] = ['width' => $this->entry['content']['width']];
+            } elseif ($section_type == 'shop' && isset($this->site_settings['shop']['entryWidth'])) {
+                $width = intval($this->site_settings['shop']['entryWidth']);
+
+                if ($width > 0) {
+                    $styles[] = ['width' => $width . 'px'];
+                }
+            }
+
+            if (!empty($styles)) {
+                $styles = array_map(function($style){
+                    $key = key($style);
+                    return $key . ': ' . ($style[$key]);
+                }, $styles);
+
+                return implode(';', $styles);
+            }
         }
-        $slug = Helpers::slugify($title, '-', '-');
 
-        return $slug;
+        return null;
     }
 
     public function render($tag = null)
