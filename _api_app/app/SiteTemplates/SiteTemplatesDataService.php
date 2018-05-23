35d5389b0067b546fc516420b0444cb7fda3041a
Viesturs Teivāns <vteivans@gmail.com>
2018-05-23 11:21:11 +0300

    Reuse SiteTemplateSettingsDataService schema

--- _api_app/app/SiteTemplates/SiteTemplatesDataService.php
+++ _api_app/app/SiteTemplates/SiteTemplatesDataService.php
@@ -17,6 +17,308 @@ use App\Shared\I18n;
  */
 class SiteTemplatesDataService
 {
+    /** @todo update schema to reflect input types etc. */
+    public static $JSON_SCHEMA = [
+        '$schema' => "http://json-schema.org/draft-06/schema#",
+        'type' => 'object',
+        'properties' => [
+            'background' => [
+                'type' => 'object',
+                'properties' => [
+                    'backgroundAttachment' => ['type' => 'string', 'enum' => ['fixed', 'fill', 'scroll']],
+                    'backgroundColor' => ['type' => 'string', 'format' => 'color'],
+                    'backgroundImage_height' => ['type' => 'integer', 'minimum' => 0],
+                    'backgroundImage_width' => ['type' => 'integer', 'minimum' => 0],
+                    'backgroundImage' => ['type' => 'string'],
+                    'backgroundImageEnabled' => ['type' => 'string', 'enum' => ['yes', 'no']],
+                    'backgroundPosition' => [
+                        'type' => 'string',
+                        'enum' => ['top left', 'top center', 'top right', 'center left', 'center', 'center right',
+                                   'bottom left', 'bottom center', 'bottom right']
+                    ],
+                    'backgroundRepeat' => [
+                        'type' => 'string', 'enum' => [ 'repeat', 'repeat-x', 'repeat-y', 'no-repeat']
+                    ]
+                ]
+            ],
+            'css' => [
+                'type' => 'object',
+                'properties' => [
+                    'customCSS' => ['type' => 'string']
+                ]
+            ],
+            'entryFooter' => [
+                'type' => 'object',
+                'properties' => [
+                    'color' => ['type' => 'string', 'format' => 'color'],
+                    'fontFamily' => ['$ref' => '#/definitions/fontFamily.df'],
+                    'fontSize' => ['$ref' => '#/definitions/cssUnit.df'],
+                    'fontStyle' => ['$ref' => '#/definitions/fontStyle.df'],
+                    'fontVariant' => ['$ref' => '#/definitions/fontVariant.df'],
+                    'fontWeight' => ['$ref' => '#/definitions/fontWeight.df'],
+                    'googleFont' => ['type' => 'string'],
+                    'lineHeight' => ['$ref' => '#/definitions/cssUnit.df'],
+                ]
+            ],
+            'entryHeading' => [
+                'type' => 'object',
+                'properties' => [
+                    'color' => ['type' => 'string', 'format' => 'color'],
+                    'fontFamily' => ['$ref' => '#/definitions/fontFamily.df'],
+                    'fontSize' => ['$ref' => '#/definitions/cssUnit.df'],
+                    'fontStyle' => ['$ref' => '#/definitions/fontStyle.df'],
+                    'fontVariant' => ['$ref' => '#/definitions/fontVariant.df'],
+                    'fontWeight' => ['$ref' => '#/definitions/fontWeight.df'],
+                    'googleFont' => ['type' => 'string'],
+                    'lineHeight' => ['$ref' => '#/definitions/cssUnit.df'],
+                    'margin' => ['$ref' => '#/definitions/cssUnit.df'],
+                ]
+            ],
+            'entryLayout' => [
+                'type' => 'object',
+                'properties' => [
+                    'contentWidth' => ['$ref' => '#/definitions/cssUnit.df'],
+                    'defaultGalleryType' => ['type' => 'string', 'enum' => ['slideshow', 'row']],
+                    'displayTags' => ['type' => 'string', 'enum' => ['yes', 'no']],
+                    'galleryMargin' => ['type' => ['string', 'integer']],
+                    'galleryNavMargin' => ['type' => ['string', 'integer']],
+                    'galleryPosition' => [
+                        'type' => 'string', 'enum' => ['between title/description', 'above title', 'below description']
+                    ],
+                    'margin' => ['$ref' => '#/definitions/cssUnit.df'],
+                    'spaceBetween' => ['$ref' => '#/definitions/cssUnit.df'],
+                    'spaceBetweenImages' => ['$ref' => '#/definitions/cssUnit.df']
+                ]
+            ],
+            'firstPage' => [
+                'type' => 'object',
+                'properties' => [
+                    'hoverWiggle' => ['type' => 'string', 'enum' => ['yes', 'no']],
+                    'imageHaveShadows' => ['type' => 'string', 'enum' => ['yes', 'no']],
+                    'imageSizeRatio' => ['type' => 'number']
+                ]
+            ],
+            'generalFontSettings' => [
+                'type' => 'object',
+                'properties' => [
+                    'color' => ['type' => 'string', 'format' => 'color'],
+                    'fontFamily' => ['$ref' => '#/definitions/fontFamily.df'],
+                    'fontSize' => ['$ref' => '#/definitions/cssUnit.df'],
+                    'fontStyle' => ['$ref' => '#/definitions/fontStyle.df'],
+                    'fontVariant' => ['$ref' => '#/definitions/fontVariant.df'],
+                    'fontWeight' => ['$ref' => '#/definitions/fontWeight.df'],
+                    'googleFont' => ['type' => 'string'],
+                    'lineHeight' => ['$ref' => '#/definitions/cssUnit.df']
+                ]
+            ],
+            'grid' => [
+                'type' => 'object',
+                'properties' => [
+                    'contentWidth' => ['$ref' => '#/definitions/cssUnit.df']
+                ]
+            ],
+            'heading' => [
+                'type' => 'object',
+                'properties' => [
+                    'color' => ['type' => 'string', 'format' => 'color'],
+                    'fontFamily' => ['$ref' => '#/definitions/fontFamily.df'],
+                    'fontSize' => ['$ref' => '#/definitions/cssUnit.df'],
+                    'fontStyle' => ['$ref' => '#/definitions/fontStyle.df'],
+                    'fontVariant' => ['$ref' => '#/definitions/fontVariant.df'],
+                    'fontWeight' => ['$ref' => '#/definitions/fontWeight.df'],
+                    'googleFont' => ['type' => 'string'],
+                    'image_height' => ['type' => 'integer', 'minimum' => 0],
+                    'image_width' => ['type' => 'integer', 'minimum' => 0],
+                    'image' => ['type' => 'string'],
+                    'lineHeight' => ['$ref' => '#/definitions/cssUnit.df'],
+                    'position' => [
+                        'type' => 'string',
+                        'enum' => ['static', 'relative', 'absolute', 'fixed', 'sticky']
+                    ]
+                ]
+            ],
+            'links' => [
+                'type' => 'object',
+                'properties' => [
+                    'colorLink' => ['type' => 'string', 'format' => 'color'],
+                    'colorVisited' => ['type' => 'string', 'format' => 'color'],
+                    'colorHover' => ['type' => 'string', 'format' => 'color'],
+                    'colorActive' => ['type' => 'string', 'format' => 'color'],
+                    'textDecorationLink' => ['$ref' => '#/definitions/textDecoration.df'],
+                    'textDecorationVisited' => ['$ref' => '#/definitions/textDecoration.df'],
+                    'textDecorationHover' => ['$ref' => '#/definitions/textDecoration.df'],
+                    'textDecorationActive' => ['$ref' => '#/definitions/textDecoration.df']
+                ]
+            ],
+            'menu' => [
+                'type' => 'object',
+                'properties' => [
+                    'colorActive' => ['type' => 'string', 'format' => 'color'],
+                    'colorHover' => ['type' => 'string', 'format' => 'color'],
+                    'colorLink' => ['type' => 'string', 'format' => 'color'],
+                    'fontFamily' =>['$ref' => '#/definitions/fontFamily.df'],
+                    'fontSize' => ['$ref' => '#/definitions/cssUnit.df'],
+                    'fontStyle' => ['$ref' => '#/definitions/fontStyle.df'],
+                    'fontVariant' => ['$ref' => '#/definitions/fontVariant.df'],
+                    'fontWeight' => ['$ref' => '#/definitions/fontWeight.df'],
+                    'googleFont' => ['type' => 'string'],
+                    'lineHeight' => ['$ref' => '#/definitions/cssUnit.df'],
+                    'margin' => ['$ref' => '#/definitions/cssUnit.df'],
+                    'position' => ['$ref' => '#/definitions/cssUnit.df'],
+                    'separator' => ['type' => 'string'],
+                    'separatorDistance' => ['$ref' => '#/definitions/cssUnit.df'],
+                    'textDecorationActive' => ['$ref' => '#/definitions/textDecoration.df'],
+                    'textDecorationHover' => ['$ref' => '#/definitions/textDecoration.df'],
+                    'textDecorationLink' => ['$ref' => '#/definitions/textDecoration.df']
+                ]
+            ],
+            'pageHeading' => [
+                'type' => 'object',
+                'properties' => [
+                    'color' => ['type' => 'string', 'format' => 'color'],
+                    'fontFamily' => ['$ref' => '#/definitions/fontFamily.df'],
+                    'fontSize' => ['$ref' => '#/definitions/cssUnit.df'],
+                    'fontStyle' => ['$ref' => '#/definitions/fontStyle.df'],
+                    'fontVariant' => ['$ref' => '#/definitions/fontVariant.df'],
+                    'fontWeight' => ['$ref' => '#/definitions/fontWeight.df'],
+                    'googleFont' => ['type' => 'string'],
+                    'image' => ['type' => 'string'],
+                    'image_height' => ['type' => 'integer'],
+                    'image_width' => ['type' => 'integer'],
+                    'lineHeight' => ['type' => 'string'],
+                    'margin' => ['$ref' => '#/definitions/cssUnit.df'],
+                    'marginBottom' => ['type' => 'string'],
+                    'marginTop' => ['type' => 'string']
+                ]
+            ],
+            'pageLayout' => [
+                'type' => 'object',
+                'properties' => [
+                    'bgButtonType' => ['type' => 'string'],
+                    'bodyMargin' => ['type' => 'string'],
+                    'centered' => ['type' => 'string'],
+                    'centeredContents' => ['type' => 'string'],
+                    'centeredWidth' => ['$ref' => '#/definitions/cssUnit.df'],
+                    'centeringGuidesColor' => ['type' => 'string'],
+                    'contentAlign' => ['type' => 'string', 'enum' => ['left', 'right', 'justify-left', 'justify-right']],
+                    'contentPosition' => ['type' => 'string', 'enum' => ['left', 'center', 'right']],
+                    'contentWidth' => ['$ref' => '#/definitions/cssUnit.df'],
+                    'headingMargin' => ['type' => 'string'],
+                    'leftColumnWidth' => ['$ref' => '#/definitions/cssUnit.df'],
+                    'mashUpColumns' => ['type' => 'integer', 'minimum' => 0],
+                    'menuMargin' => ['type' => 'string'],
+                    'paddingLeft' => ['$ref' => '#/definitions/cssUnit.df'],
+                    'paddingTop' => ['$ref' => '#/definitions/cssUnit.df'],
+                    'responsive' => ['type' => 'string', 'enum' => ['no', 'yes']],
+                    'siteMenuMargin' => ['$ref' => '#/definitions/cssUnit.df']
+                ]
+            ],
+            'sideBar' => [
+                'type' => 'object',
+                    'properties' => [
+                    'backgroundColor' => ['type' => 'string', 'format' => 'color'],
+                    'color' => ['type' => 'string', 'format' => 'color'],
+                    'fontFamily' => ['$ref' => '#/definitions/fontFamily.df'],
+                    'fontSize' => ['$ref' => '#/definitions/cssUnit.df'],
+                    'fontStyle' => ['$ref' => '#/definitions/fontStyle.df'],
+                    'fontVariant' => ['$ref' => '#/definitions/fontVariant.df'],
+                    'fontWeight' => ['$ref' => '#/definitions/fontWeight.df'],
+                    'googleFont' => ['type' => 'string'],
+                    'image' => ['type' => 'string'],
+                    'image_height' => ['type' => 'integer'],
+                    'image_width' => ['type' => 'integer'],
+                    'lineHeight' => ['$ref' => '#/definitions/cssUnit.df'],
+                    'marginBottom' => ['$ref' => '#/definitions/cssUnit.df'],
+                    'marginLeft' => ['$ref' => '#/definitions/cssUnit.df'],
+                    'marginTop' => ['$ref' => '#/definitions/cssUnit.df'],
+                    'transparent' => ['type' => 'string', 'enum' => ['yes', 'no']],
+                    'width' => ['$ref' => '#/definitions/cssUnit.df']
+                ]
+            ],
+            'subMenu' => [
+                'type' => 'object',
+                'properties' => [
+                    'fontFamily' => ['$ref' => '#/definitions/fontFamily.df'],
+                    'fontSize' => ['$ref' => '#/definitions/cssUnit.df'],
+                    'fontStyle' => ['$ref' => '#/definitions/fontStyle.df'],
+                    'fontVariant' => ['$ref' => '#/definitions/fontVariant.df'],
+                    'fontWeight' => ['$ref' => '#/definitions/fontWeight.df'],
+                    'googleFont' => ['type' => 'string'],
+                    'lineHeight' => ['$ref' => '#/definitions/cssUnit.df'],
+                    'margin' => ['$ref' => '#/definitions/cssUnit.df'],
+                    'separator' => ['type' => 'string'],
+                    'separatorDistance' => ['$ref' => '#/definitions/cssUnit.df']
+                ]
+            ],
+            'tagsMenu' => [
+                'type' => 'object',
+                'properties' => [
+                    'fontFamily' => ['$ref' => '#/definitions/fontFamily.df'],
+                    'googleFont' => ['type' => 'string'],
+                    'fontSize' => ['$ref' => '#/definitions/cssUnit.df'],
+                    'fontWeight' => ['$ref' => '#/definitions/fontWeight.df'],
+                    'fontStyle' => ['$ref' => '#/definitions/fontStyle.df'],
+                    'lineHeight' => ['$ref' => '#/definitions/cssUnit.df'],
+                    'colorLink' => ['type' => 'string', 'format' => 'color'],
+                    'colorHover' => ['type' => 'string', 'format' => 'color'],
+                    'colorActive' => ['type' => 'string', 'format' => 'color'],
+                    'textDecorationLink' => ['$ref' => '#/definitions/textDecoration.df'],
+                    'textDecorationHover' => ['$ref' => '#/definitions/textDecoration.df'],
+                    'textDecorationActive' => ['$ref' => '#/definitions/textDecoration.df'],
+                    'x' => ['type' => 'integer'],
+                    'y' => ['type' => 'integer'],
+                    'alwaysOpen' => ['type' => 'string', 'enum' => ['yes', 'no']],
+                    'hidden' => ['type' => 'string', 'enum' => ['yes', 'no']]
+                ]
+            ]
+        ],
+        'definitions' => [
+            /**
+             * Define these types only once, and reuse them, because they are used many times.
+             * - The ".df" suffix is for easier searching, it is not required by the json schema.
+             * - Enum values are extracted from "template.conf.php"
+             */
+            'fontWeight.df' => [
+                'type' => 'string',
+                'enum' => ['normal', 'bold', 'bolder', 'light', 'lighter', 'inherit']
+            ],
+            'fontFamily.df' => [
+                'type' => 'string',
+                'enum' => [
+                    'Arial, sans-serif',
+                    'Helvetica, Arial, sans-serif',
+                    '"Helvetica Neue", Helvetica, Arial, sans-serif',
+                    '"Arial Black", Gadget, sans-serif',
+                    '"Comic Sans MS", cursive',
+                    '"Courier New", Courier, monospace',
+                    'Georgia, "Times New Roman", Times, serif',
+                    'Impact, Charcoal, sans-serif',
+                    '"Lucida Console", Monaco, monospace',
+                    '"Lucida Sans Unicode", "Lucida Grande", sans-serif',
+                    '"Palatino Linotype", "Book Antiqua", Palatino, serif',
+                    'Tahoma, Geneva, sans-serif',
+                    '"Times New Roman", Times, serif',
+                    '"Trebuchet MS", Helvetica, sans-serif',
+                    'Verdana, Geneva, sans-serif'
+                ]
+            ],
+            'fontStyle.df' => [  // https://developer.mozilla.org/en-US/docs/Web/CSS/font-style
+                'type' => 'string', 'enum' => ['normal', 'italic', 'oblique', 'inherit']
+            ],
+            'fontVariant.df' => [
+                'type' => 'string', 'enum' => ['small-caps', 'inherit', 'normal']
+            ],
+            'textDecoration.df' => [
+                'type' => 'string',
+                'enum' => ['none', 'underline', 'overline', 'line-through']
+            ],
+            'cssUnit.df' => [
+                'type' => ['string', 'integer'],
+                'format' => 'css-unit'
+            ]
+        ]
+    ];
+
     private $TEMPLATE_ROOT;
 
     public function __construct()
