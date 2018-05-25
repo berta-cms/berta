52add04c4c19dce439842f3a8bb5fed65701cbb1
Viesturs Teivāns <vteivans@gmail.com>
2018-05-25 12:12:27 +0300

    Example added and types updated to reflect the actual situation

--- _api_app/app/SiteTemplates/SiteTemplatesDataService.php
+++ _api_app/app/SiteTemplates/SiteTemplatesDataService.php
@@ -11,6 +11,115 @@ use App\Shared\I18n;
  * representing \App\Site\SiteTemplateSettings. SiteTemplateSettings for any specific site are generated according
  * to it's template.
  *
+ * @example This example is shown in JSON form. The actual definitions are in PHP at this time.
+ *  "default": {
+ *      "templateConf": {
+ *          "generalFontSettings": {
+ *              "_": {
+ *                  "title": "General font settings"
+ *              },
+ *              "color": {
+ *                  "format": "color",
+ *                  "default": "#333333",
+ *                  "title": "Color",
+ *                  "description": ""
+ *              },
+ *              "fontFamily": {
+ *                  "format": "fontselect",
+ *                  "values": [
+ *                      "Helvetica, Arial, sans-serif",
+ *                      "\"Helvetica Neue\", Helvetica, Arial, sans-serif",
+ *                      "\"Arial Black\", Gadget, sans-serif",
+ *                      "\"Comic Sans MS\", cursive",
+ *                      "\"Courier New\", Courier, monospace",
+ *                      "Georgia, \"Times New Roman\", Times, serif",
+ *                      "Impact, Charcoal, sans-serif",
+ *                      "\"Lucida Console\", Monaco, monospace",
+ *                      "\"Lucida Sans Unicode\", \"Lucida Grande\", sans-serif",
+ *                      "\"Palatino Linotype\", \"Book Antiqua\", Palatino, serif",
+ *                      "Tahoma, Geneva, sans-serif",
+ *                      "\"Times New Roman\", Times, serif",
+ *                      "\"Trebuchet MS\", Helvetica, sans-serif",
+ *                      "Verdana, Geneva, sans-serif"
+ *                  ],
+ *                  "default": "Helvetica, Arial, sans-serif",
+ *                  "title": "Font face",
+ *                  "description": ""
+ *              },
+ *              ...
+ *          },
+ *          "background": {
+ *              "_": {
+ *                  "title": "Background"
+ *              },
+ *              ...,
+ *              "backgroundRepeat": {
+ *                  "format": "select",
+ *                  "values": {
+ *                      "repeat": "tile vertically and horizontally",
+ *                      "repeat-x": "tile horizontally",
+ *                      "repeat-y": "tile vertically",
+ *                      "no-repeat": "no tiling"
+ *                  },
+ *                  "default": "repeat",
+ *                  "title": "Background tiling",
+ *                  "description": "How the background fills the screen?"
+ *              }
+ *              ...
+ *          },
+ *          ...
+ *      },
+ *      "sectionTypes": {
+ *          "default": {
+ *              "title": "Default",
+ *              "params": {
+ *                  "columns": {
+ *                      "format": "select",
+ *                      "default": "1",
+ *                      "values": [
+ *                          "1",
+ *                          "2",
+ *                          "3",
+ *                          "4"
+ *                      ],
+ *                      "html_before": "<div class=\"label\">Columns:<\/div>"
+ *                  },
+ *                  ...,
+ *                  "backgroundVideoRatio": {
+ *                      "format": "select",
+ *                      "default": "fillWindow",
+ *                      "values": {
+ *                          "fillWindow": "Fill window",
+ *                          "keepRatio": "Keep ratio"
+ *                      },
+ *                      "html_before": "<div class=\"label\">Background video ratio:<\/div>"
+ *                  }
+ *              }
+ *          },
+ *          "external_link": {
+ *              "title": "External link",
+ *              "params": {
+ *                  "link": {
+ *                      "format": "text",
+ *                      "default": "",
+ *                      "link": true,
+ *                      "html_before": "<div class=\"label\">Link address:<\/div>"
+ *                  },
+ *                  "target": {
+ *                      "format": "select",
+ *                      "values": {
+ *                          "_self": "Same window",
+ *                          "_blank": "New window"
+ *                      },
+ *                      "default": "_blank",
+ *                      "html_before": "<div class=\"label\">Opens in:<\/div>"
+ *                  }
+ *              }
+ *          },
+ *          ...
+ *      }
+ *  }
+ *
  * Template configurations can be found at `berta/_templates/[template name]/template.conf.php`.
  * @todo: Templates should be moved to this folder.
  * @todo: Template settings should be moved to XML so we have everything in a standardized way.
@@ -33,14 +142,14 @@ class SiteTemplatesDataService
                 'properties' => [
                     'templateConf' => [
                         'type' => 'object',
-                        'additionalProperties' => [  // generalFontSettings, links, background...
+                        'additionalProperties' => [  // generalFontSettings, links, background, ...
                             'type' => 'object',
                             'properties' => [
                                 "_" => [
                                     'title' => ['type' => 'string']
                                 ]
                             ],
-                            'additionalProperties' => [  // _, color, fontFamily, googleFont...
+                            'additionalProperties' => [  // _, color, fontFamily, backgroundRepeat, ...
                                 'type' => 'object',
                                 'properties' => [
                                     'default' => ['type' => ['string', 'number']],
@@ -60,32 +169,41 @@ class SiteTemplatesDataService
                                     'max_width' => ['type' => ['number', 'string']],
                                     'max_height' => ['type' => ['number', 'string']],
                                     'allow_blank' => ['type' => 'boolean'],
-                                    'params' => [
+                                ]
+                            ]
+                        ]
+                    ],
+                    'sectionTypes' => [
+                        'type' => 'object',
+                        'required' => ['default'],
+                        'additionalProperties' => [  // default, external_links, portfolio, ...
+                            'type' => 'object',
+                            'properties' => [
+                                'title' => ['type' => 'string'],
+                                'params' => [
+                                    'type' => 'object',
+                                    'additionalProperties' => [  // columns, backgroundVideoRatio, link, target, ...
                                         'type' => 'object',
-                                        '$comment' => 'any kind of parameters required for this field',
-                                        'additionalProperties' => [  // link, target, marked_items_count...
-                                            'type' => 'object',
-                                            'properties' => [
-                                                'default' => ['type' => ['string', 'number']],
-                                                'description' => ['type' => 'string'],
-                                                'title' => ['type' => 'string'],
-                                                'format' => ['type' => 'string'],
-                                                'values' => [
-                                                    'anyOf' => [
-                                                        ['type' => 'array', 'items' => ['string', 'number']],
-                                                        ['type' => 'object', 'additionalProperties' => ['type' => 'string', 'number']]
-                                                    ]
-                                                ],
-                                                'html_entities' => ['type' => 'boolean'],
-                                                'css_units' => ['type' => 'boolean'],
-                                                'min_width' => ['type' => ['number', 'string']],
-                                                'min_height' => ['type' => ['number', 'string']],
-                                                'max_width' => ['type' => ['number', 'string']],
-                                                'max_height' => ['type' => ['number', 'string']],
-                                                'allow_blank' => ['type' => 'boolean'],
-                                                'html_before' => ['type' => 'string'],
-                                                'html_after' => ['type' => 'string']
-                                            ]
+                                        'properties' => [
+                                            'default' => ['type' => ['string', 'number']],
+                                            'description' => ['type' => 'string'],
+                                            'title' => ['type' => 'string'],
+                                            'format' => ['type' => 'string'],
+                                            'values' => [
+                                                'anyOf' => [
+                                                    ['type' => 'array', 'items' => ['string', 'number']],
+                                                    ['type' => 'object', 'additionalProperties' => ['type' => 'string', 'number']]
+                                                ]
+                                            ],
+                                            'html_entities' => ['type' => 'boolean'],
+                                            'css_units' => ['type' => 'boolean'],
+                                            'min_width' => ['type' => ['number', 'string']],
+                                            'min_height' => ['type' => ['number', 'string']],
+                                            'max_width' => ['type' => ['number', 'string']],
+                                            'max_height' => ['type' => ['number', 'string']],
+                                            'allow_blank' => ['type' => 'boolean'],
+                                            'html_before' => ['type' => 'string'],
+                                            'html_after' => ['type' => 'string']
                                         ]
                                     ]
                                 ]
@@ -95,10 +213,6 @@ class SiteTemplatesDataService
                 ]
             ]
         ]
-    ];
-
-    private $SETTINGS = [
-
     ];
 
     private $TEMPLATE_ROOT;
