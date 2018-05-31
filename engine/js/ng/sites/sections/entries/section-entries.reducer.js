42f3b29b4c1906cd0e0c09b83733b3bf191c1643
Uldis Rudzitis <uldis.rudzitis@gmail.com>
2018-05-31 13:04:46 +0300

    Reducer for entry update

--- engine/js/ng/sites/sections/entries/section-entries.reducer.js
+++ engine/js/ng/sites/sections/entries/section-entries.reducer.js
@@ -14,8 +14,30 @@
       switch (action.type) {
 
         case ActionTypes.SET_STATE:
-          console.log('Entries reducer:', action);
           return Immutable.fromJS(action.state.sectionEntries);
+
+
+        case ActionTypes.UPDATE_SECTION_ENTRY:
+          var path = action.resp.path.split('/');
+          var prop = action.resp.path.split('/').slice(4);
+          var siteName = path[0];
+          var sectionName = path[2];
+          var entryId = path[3];
+          var value = action.resp.value;
+
+          var index = state.get(siteName).findIndex(function (entry) {
+            return entry.get('id') === entryId && entry.get('sectionName') === sectionName;
+          });
+
+          if (index < 0) {
+            return state;
+          }
+
+          return state.setIn(
+            [siteName, index].concat(prop),
+            value
+          );
+
 
         // case ActionTypes.SECTION_CREATED:
         //   console.log('Entries reducer:', action);
