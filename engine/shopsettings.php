3c383549bb54d7d4b710abdcb043675e6a7aa863
Uldis Rudzitis <uldis.rudzitis@gmail.com>
2018-06-29 10:31:15 +0300

    PHP CS

--- engine/shopsettings.php
+++ engine/shopsettings.php
@@ -1,10 +1,10 @@
 <?php
 define('AUTH_AUTHREQUIRED', true);
 define('BERTA_ENVIRONMENT', 'engine');
-include('inc.page.php');
+include 'inc.page.php';
 $loggedIn = $berta->security->userLoggedIn;
 include_once $ENGINE_ROOT . '_classes/class.bertaeditor.php';
 
 if ($shopEnabled) {
-	include('../_plugin_shop/shopsettings.php');
+    include '../_plugin_shop/shopsettings.php';
 }
