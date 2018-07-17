20afe7cb0b0737d2c9213158e436e8558b957e15
Viesturs Teivāns <vteivans@gmail.com>
2018-07-17 13:48:25 +0300

    Random component removed

--- editor/src/app/app.module.ts
+++ editor/src/app/app.module.ts
@@ -8,7 +8,6 @@ import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { RandomComponent } from './random/random.component';
 import { AppState } from './app-state/app.state';
 import { HeaderComponent } from './header/header.component';
 import { ProfileDropdownComponent } from './profile-dropdown/profile-dropdown.component';
@@ -18,7 +17,6 @@ import { SectionsComponent } from './sections/sections.component';
   declarations: [
     AppComponent,
     NotFoundComponent,
-    RandomComponent,
     HeaderComponent,
     ProfileDropdownComponent,
     SectionsComponent
