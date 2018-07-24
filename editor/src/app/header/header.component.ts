38cd5169ebb200ae42bbcbbd52c7367bb69975c1
Viesturs Teivāns <vteivans@gmail.com>
2018-07-24 10:48:24 +0300

    Redundant navigation item revmoed
    + Navigation items sorted according to their hairarchy

--- editor/src/app/header/header.component.ts
+++ editor/src/app/header/header.component.ts
@@ -11,12 +11,11 @@ import { AppState } from '../app-state/app.state';
     <header>
       <div class="bt-menu" *ngIf="isLoggedIn$ | async">
         <nav>
-          <a [routerLink]="['/sections']" [routerLinkActive]="'nav-active'" queryParams="">Sections</a>
-          <a [routerLink]="['/design']" [routerLinkActive]="'nav-active'" queryParams="">Design</a>
-          <a [routerLink]="['/settings']" [routerLinkActive]="'nav-active'" queryParams="">Settings</a>
           <a [routerLink]="['/multisite']" [routerLinkActive]="'nav-active'" queryParams="">Multisite</a>
+          <a [routerLink]="['/sections']" [routerLinkActive]="'nav-active'" queryParams="">Sections</a>
+          <a [routerLink]="['/settings']" [routerLinkActive]="'nav-active'" queryParams="">Settings</a>
+          <a [routerLink]="['/design']" [routerLinkActive]="'nav-active'" queryParams="">Design</a>
           <a [routerLink]="['/shop']" [routerLinkActive]="'nav-active'" queryParams="">Shop</a>
-          <a [routerLink]="['/seo']" [routerLinkActive]="'nav-active'" queryParams="">Seo</a>
           <a href="http://support.berta.me/kb" target="_blank">Knowledge base</a>
         </nav>
         <berta-profile-dropdown></berta-profile-dropdown>
