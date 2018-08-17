e2969e7fa376a8e3f33bb3fc83d60069798aad13
Uldis Rudzitis <uldis.rudzitis@gmail.com>
2018-08-17 15:08:14 +0300

    Example data and result for `objectToPathArray` method. Remove unused `slugify` method.

--- editor/src/app/shared/helpers.ts
+++ editor/src/app/shared/helpers.ts
@@ -45,26 +45,34 @@ export function isPlainObject(obj: any): boolean {
   return false;
 }
 
-export function slugify(str: string): string {
-  str = str.replace(/^\s+|\s+$/g, ''); // trim
-  str = str.toLowerCase();
-
-  // remove accents, swap ñ for n, etc
-  const from = 'àáäâèéëêìíïîòóöôùúüûñç·/_,:;';
-  const to   = 'aaaaeeeeiiiioooouuuunc------';
-  for (let i = 0, l = from.length; i < l; i++) {
-      str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
-  }
-
-  str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
-      .replace(/\s+/g, '-') // collapse whitespace and replace by -
-      .replace(/-+/g, '-'); // collapse dashes
-
-  return str;
-}
 
 /**
- * Transforms payload object into flat array with key path and value
+ * Transforms deep payload object into flat array with key path and value
+ *
+ * Example object:
+ * {
+ *   name: 'John'
+ *   attributes: {
+ *     age: 18,
+ *     weight: 75
+ *   }
+ * }
+ *
+ * Result:
+ * [
+ *   {
+ *     path: ['name'],
+ *     value: 'John'
+ *   },
+ *   {
+ *     path: ['attributes', 'age'],
+ *     value: 18
+ *   },
+ *   {
+ *     path: ['attributes', 'weight'],
+ *     value: 75
+ *   }
+ * ]
  *
  * @param payload object to convert
  */
