340b6be0b7538150ad6defdcc1ea27bc36f016a2
Uldis Rudzitis <uldis.rudzitis@gmail.com>
2018-09-05 13:17:47 +0300

    Fix editor redirect in apache config

--- _preview/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('AUTH_AUTHREQUIRED', true);  // require authentification if inside engine folder
-define('BERTA_ENVIRONMENT', 'engine');
-
-include '../engine/inc.page.php';
-include '../engine/editor/inc.editor.php';
