f1a70af7e59865bf501731470d90f175810a6cac
Uldis Rudzitis <uldis.rudzitis@gmail.com>
2018-09-05 12:57:31 +0300

    Update value only if changed

--- editor/src/app/inputs/text-input.component.ts
+++ editor/src/app/inputs/text-input.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { SettingModel } from '../shared/interfaces';
 
 @Component({
   selector: 'berta-text-input',
@@ -7,22 +8,34 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
       <label>
         {{ label }}
         <input [value]="value"
-              (keydown)="updateField($event)"
-              (blur)="updateField($event)"
-              type="text">
+               (keydown)="updateField($event)"
+               (blur)="updateField($event)"
+               type="text">
       </label>
     </div>`
 })
-export class TextInputComponent {
+export class TextInputComponent implements OnInit {
   @Input() label: string;
   @Input() value: string;
   @Output() update = new EventEmitter();
 
+  private lastValue: SettingModel['value'];
+
+  ngOnInit() {
+    // Cache the value, so we don't update if nothing changes
+    this.lastValue = this.value;
+  }
+
   updateField($event) {
-    // @todo: must fix - keydown ENTER event also fires blur event, this fires event twice
+    if ($event.target.value === this.lastValue) {
+      return;
+    }
+
     if ($event instanceof KeyboardEvent && !($event.key === 'Enter' || $event.keyCode === 13)) {
       return;
     }
+
+    this.lastValue = $event.target.value;
     this.update.emit($event.target);
   }
 }
