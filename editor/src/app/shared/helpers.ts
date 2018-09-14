b92f6fa7c7638b3b375c5a4df1194b9740441fdd
Uldis Rudzitis <uldis.rudzitis@gmail.com>
2018-09-14 12:55:06 +0300

    Fix toggle detection condition

--- editor/src/app/shared/helpers.ts
+++ editor/src/app/shared/helpers.ts
@@ -140,8 +140,7 @@ export function initSettingConfigGroup(settingGroupConfigResponse: SettingConfig
 
       // Turn ['yes', 'no'] select type into toggle input type
       if (values.length === 2 &&
-         (values[0].value === 'yes' || values[0].value === 'no') &&
-         (values[1].value === 'yes' || values[1].value === 'no')) {
+          ((values[0].value === 'yes' && values[1].value === 'no') || (values[0].value === 'no' && values[1].value === 'yes'))) {
         settingGroupConfigResponse[settingSlug].format = 'toggle';
       }
 
