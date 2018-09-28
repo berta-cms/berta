45e38cf68875d2d8d7364a966505df43e875a111
Uldis Rudzitis <uldis.rudzitis@gmail.com>
2018-09-28 11:25:53 +0300

    Placeholder color and margin between icon

--- editor/src/app/inputs/text-input.component.ts
+++ editor/src/app/inputs/text-input.component.ts
@@ -10,7 +10,7 @@ import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
       <label>
         {{ label }}
         <div class="text-input-wrapper">
-          <svg *ngIf="showIcon" width="20" height="20" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
+          <svg *ngIf="showIcon" class="icon-empty" width="20" height="20" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
             <path class="icon" d="M6 34.5v7.5h7.5l22.13-22.13-7.5-7.5-22.13 22.13zm35.41-20.41c.78-.78.78-2.05 0-2.83l-4.67-4.67c-.78-.78-2.05-.78-2.83 0l-3.66 3.66 7.5 7.5 3.66-3.66z"/>
             <path d="M0 0h48v48h-48z" fill="none"/>
           </svg>
