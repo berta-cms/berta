0e319b62d869b401cc50a21cf1d3d701932198ad
Uldis Rudzitis <uldis.rudzitis@gmail.com>
2018-10-01 17:11:53 +0300

    Shop order panel

--- editor/src/app/shop/orders/shop-orders.component.ts
+++ editor/src/app/shop/orders/shop-orders.component.ts
@@ -5,21 +5,10 @@ import { Select } from '@ngxs/store';
 @Component({
   selector: 'berta-shop-orders',
   template: `
-    <div *ngFor="let order of orders$ | async">
+    <div *ngFor="let order of orders$ | async" class="setting">
       <p>{{order.id}}. <span>{{order.created_at}}</span></p>
     </div>
-  `,
-  styles: [`
-  p {
-    font-size: 0.875em;
-    color: #9b9b9b;
-    display: flex;
-    justify-content: space-between;
-  }
-  p > span {
-    width: 50%;
-  }
-`]
+  `
 })
 export class ShopOrdersComponent implements OnInit {
   @Select(ShopOrdersState.getCurrentSiteOrders) orders$;
@@ -28,5 +17,4 @@ export class ShopOrdersComponent implements OnInit {
 
   ngOnInit() {
   }
-
 }
