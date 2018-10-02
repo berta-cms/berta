ffdf2b434bba78c70f43281f950ece04d5eb21f6
Uldis Rudzitis <uldis.rudzitis@gmail.com>
2018-10-02 12:37:06 +0300

    Fix: identify product by id, uniqid is a reference to entry

--- editor/src/app/shop/products/shop-products.actions.ts
+++ editor/src/app/shop/products/shop-products.actions.ts
@@ -1,7 +1,7 @@
 export class UpdateShopProductAction {
   static readonly type = 'SHOP_PRODUCT:UPDATE';
   constructor(
-    public uniqid: string,
+    public id: string,
     public payload: {
       field: string,
       value: any
