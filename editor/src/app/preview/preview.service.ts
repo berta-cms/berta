fb3b859d4748a81e8a359506d624f7de2649e202
Viesturs Teivāns <vteivans@gmail.com>
2018-10-10 11:38:44 +0300

    more action data analyse

--- editor/src/app/preview/preview.service.ts
+++ editor/src/app/preview/preview.service.ts
@@ -18,32 +18,60 @@ export class PreviewService {
     console.log('SYNC DATA:', data);
     console.log('SYNC METHOD:', method);
 
-    if (data.path && data.path.startsWith('/settings')) {
-      /* trigger setting update actions */
+    const urlParts = this.parseSyncUrl(url);
+    const urlIdentifier = urlParts.join('/');
+
+    switch (urlIdentifier) {
+      case 'sites/settings':
+        /** trigger setting update actions
+         * @example:
+        SYNC URL: http://local.berta.me/_api/v1/sites/settings
+        SYNC DATA: {
+          path: "/settings/siteTexts/additionalText",
+          value: "<p><strong>FOOTER TEXXXT</strong></p>↵<p></p>"
+        }
+        SYNC METHOD: PATCH
+        */
+        break;
+
+        case 'sites/sections':
+        /** trigger section update actions like gallery properties
+         * @example:
+        SYNC URL: http://local.berta.me/_api/v1/sites/sections
+        SYNC DATA: {path: "0/section/0/mediaCacheData/@attributes/autoplay", value: "31"}
+        SYNC METHOD: PATCH
+        */
+        break;
+
+        case 'sites/sections/backgrounds':
+        /* Background has its own endpoint
+          SYNC URL: http://local.berta.me/_api/v1/sites/sections/backgrounds
+          preview.service.ts:18 SYNC DATA: {site: "0", section: "maig", file: "chrome_2018-03-21_15-39-07.jpg"}
+          preview.service.ts:19 SYNC METHOD: DELETE
+        */
+
+        case 'sites/sections/entries':
+        /* trigger entry update actions */
+        break;
     }
-
-    if (data.path && data.path.startsWith('/entry')) {
-      /* trigger entry update actions */
-    }
-
-    /*
-
-
-    SYNC METHOD: DELETE
-    preview.service.ts:16 Sync through angular
-    preview.service.ts:17 SYNC URL: http://local.berta.me/_api/v1/sites/sections/backgrounds
-    preview.service.ts:18 SYNC DATA: {site: "", section: "maig", files: Array(2)}files: (2) ["tumblr-n76fa8bp2p1s3cyypo1-1280_2.png_1000x1000.png", "phone-girl.png_434x854.png"]section: "maig"site: ""__proto__: Object
-    preview.service.ts:19 SYNC METHOD: PUT
-    preview.service.ts:16 Sync through angular
-    preview.service.ts:17 SYNC URL: http://local.berta.me/_api/v1/sites/sections/backgrounds
-    preview.service.ts:18 SYNC DATA: {site: "", section: "maig", files: Array(3)}files: (3) ["tumblr-n76fa8bp2p1s3cyypo1-1280_2.png_1000x1000.png", "phone-girl.png_434x854.png", "chrome_2018-03-21_15-39-07.jpg"]section: "maig"site: ""__proto__: Object
-    preview.service.ts:19 SYNC METHOD: PUT
-    */
 
     /** @todo:
      * - Trigger appropriate actions to update the state here instead of direct sync
      * - Trigger correct actions to update the iframe when settings change
      */
     return this.appService.sync(url, data, method);
+  }
+
+  parseSyncUrl(url) {
+    const parts = url.split('/');
+    if (parts.length > 0 && /^https?:$/.test(parts[0])) {
+      // http://local.berta.me/_api/v1/sites/sections => ["http:", "", "local.berta.me", "_api", "v1", "sites", "sections"]
+      return parts.slice(5);
+    }
+    if (parts[0] === '') {
+      // /sites/sections => ["", "sites", "sections"]
+      return parts.slice(1);
+    }
+    return parts;
   }
 }
