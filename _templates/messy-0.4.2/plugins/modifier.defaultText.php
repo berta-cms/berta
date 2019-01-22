036209c80acbd192c87804e00fd6c148f901341b
Uldis Rudzitis <uldis.rudzitis@gmail.com>
2019-01-22 15:54:39 +0200

    Restore Smarty modifier defaultText: it is used in shop plugin

---
+++ _templates/messy-0.4.2/plugins/modifier.defaultText.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Smarty plugin
+ * -------------------------------------------------------------
+ * File:     modifier.defaultText.php
+ * Type:     modifier
+ * Name:     defaultText
+ * Purpose:  shows text from xml or shows default text, if not defined
+ * -------------------------------------------------------------
+ */
+function smarty_modifier_defaultText($text, $defaultText)
+{
+    global $berta;
+
+    if ($text) {
+        return $text;
+    }
+
+    return $defaultText;
+}
