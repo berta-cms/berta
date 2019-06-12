8bbab04c43e5e420f26568a567199aaf5db1f313
Viesturs Teivans <vteivans@gmail.com>
2019-06-12 12:17:07 +0300

    Fix styling of 404 page
    - Non-existing php file replaced with existing CSS file
    - Show 404 as heading if site heading is not set

--- engine/error/404.php
+++ engine/error/404.php
@@ -6,11 +6,13 @@ header("HTTP/1.0 404 Not Found");
     <meta charset="UTF-8">
 	<title><?php echo $berta->settings->get('texts', 'page-title') ?></title>
 	<link rel="SHORTCUT ICON" href="<?php echo $SITE_ROOT_URL ?>favicon.ico">
-	<link rel="stylesheet" href="<?php echo $ENGINE_ROOT_URL ?>css/id.css.php" type="text/css">
+	<link rel="stylesheet" href="<?php echo $ENGINE_ROOT_URL ?>css/frontend.min.css" type="text/css">
 </head>
 <body>
 	<div id="allContainer">
-		<h1 id="allPageTitle"><a href="./"><?php echo $berta->settings->get('site-texts', 'site-heading') ?></a></h1>
+        <h1 id="allPageTitle"><a href="/"><?php echo $berta->settings->get('site-texts', 'site-heading')
+            ? $berta->settings->get('site-texts', 'site-heading')
+            : '404' ?></a></h1>
 		<p>Page not found!</p>
 	</div>
 </body>
