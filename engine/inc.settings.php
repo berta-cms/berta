618a17abe18fd41801c0fc31cae4a5ce9b4ccbe9
Uldis Rudzitis <uldis.rudzitis@gmail.com>
2019-09-06 11:47:55 +0300

    Reorder site settings

--- engine/inc.settings.php
+++ engine/inc.settings.php
@@ -64,15 +64,6 @@ $settingsDefinition = [
         'metaKeywords' => ['format' => 'text', 'allow_blank' => true, 'default' => 'berta', 'title' => I18n::_('<META> keywords'), 'description' => I18n::_('Keywords visible only to search engines. Keywords along with the description can improve your site ranking in search results.')],
     ],
 
-    'navigation' => [
-        '_' => ['title' => I18n::_('Navigation')],
-        'landingSectionVisible' => ['format' => 'select', 'default' => 'yes', 'values' => ['yes', 'no'], 'title' => I18n::_('Is first section visible in menu?'), 'description' => I18n::_('Choose "no" to hide the first section in the main menu. Link from the page title (or header image) will lead to it. NOTE: This setting has no effect, if the section has a submenu; then it is visible at all times.')],
-        'landingSectionPageHeadingVisible' => ['format' => 'select', 'default' => 'yes', 'values' => ['yes', 'no'], 'title' => I18n::_('Show page heading in first section?'), 'description' => I18n::_('Choose "no" to hide page heading in first section.')],
-        'landingSectionMenuVisible' => ['format' => 'select', 'default' => 'yes', 'values' => ['yes', 'no'], 'title' => I18n::_('Show menu in first section?'), 'description' => I18n::_('Choose "no" to hide the menu in first section.')],
-        'alwaysSelectTag' => ['format' => 'select', 'default' => 'yes', 'values' => ['yes', 'no'], 'title' => I18n::_('Always auto-select a submenu item?'), 'description' => I18n::_('Choose "yes" to automatically select the first submenu item when clicking on a menu item. This works only when there is a submenu.')],
-        'backToTopEnabled' => ['format' => 'select', 'default' => 'no', 'values' => ['yes', 'no'], 'title' => I18n::_('Enable navigation to page top'), 'description' => I18n::_('Shows navigation button at the bottom when page is scrolled.')],
-    ],
-
     'pageLayout' => [
         '_' => ['title' => I18n::_('Page layout')],
         'favicon' => ['format' => 'icon', 'default' => '', 'title' => I18n::_('Favicon'), 'description' => I18n::_('Small picture to display in the address bar of the browser. The file must be in .ICO format and 16x16 pixels big.')],
@@ -81,14 +72,13 @@ $settingsDefinition = [
         'gridColor' => ['format' => 'select', 'default' => 'black', 'values' => ['black', 'white'], 'title' => I18n::_('Gridlines color'), 'description' => ''],
     ],
 
-    'media' => [
-        '_' => ['title' => I18n::_('Media')],
-        'imagesSmallWidth' => ['format' => 'text', 'default' => $options['images']['small_width'], 'css_units' => false, 'title' => I18n::_('Small image width'), 'description' => I18n::_('Maximum size of a small image (visible if \'Small images\' are switched on in the gallery editor). These settings don\'t affect original image.')],
-        'imagesSmallHeight' => ['format' => 'text', 'default' => $options['images']['small_height'], 'css_units' => false, 'title' => I18n::_('Small image height'), 'description' => ''],
-        'imagesMediumWidth' => ['format' => 'text', 'default' => $options['images']['medium_width'], 'css_units' => false, 'title' => I18n::_('Medium image width'), 'description' => I18n::_('Maximum size of a medium image (visible if \'Medium images\' are switched on in the gallery editor). These settings don\'t affect original image.')],
-        'imagesMediumHeight' => ['format' => 'text', 'default' => $options['images']['medium_height'], 'css_units' => false, 'title' => I18n::_('Medium image height'), 'description' => ''],
-        'imagesLargeWidth' => ['format' => 'text', 'default' => $options['images']['large_width'], 'css_units' => false, 'title' => I18n::_('Large image width'), 'description' => I18n::_('Maximum size of a large image (visible if \'Large images\' are switched on in the gallery editor). These settings don\'t affect original image.')],
-        'imagesLargeHeight' => ['format' => 'text', 'default' => $options['images']['large_height'], 'css_units' => false, 'title' => I18n::_('Large image height'), 'description' => ''],
+    'navigation' => [
+        '_' => ['title' => I18n::_('Navigation')],
+        'landingSectionVisible' => ['format' => 'select', 'default' => 'yes', 'values' => ['yes', 'no'], 'title' => I18n::_('Is first section visible in menu?'), 'description' => I18n::_('Choose "no" to hide the first section in the main menu. Link from the page title (or header image) will lead to it. NOTE: This setting has no effect, if the section has a submenu; then it is visible at all times.')],
+        'landingSectionPageHeadingVisible' => ['format' => 'select', 'default' => 'yes', 'values' => ['yes', 'no'], 'title' => I18n::_('Show page heading in first section?'), 'description' => I18n::_('Choose "no" to hide page heading in first section.')],
+        'landingSectionMenuVisible' => ['format' => 'select', 'default' => 'yes', 'values' => ['yes', 'no'], 'title' => I18n::_('Show menu in first section?'), 'description' => I18n::_('Choose "no" to hide the menu in first section.')],
+        'alwaysSelectTag' => ['format' => 'select', 'default' => 'yes', 'values' => ['yes', 'no'], 'title' => I18n::_('Always auto-select a submenu item?'), 'description' => I18n::_('Choose "yes" to automatically select the first submenu item when clicking on a menu item. This works only when there is a submenu.')],
+        'backToTopEnabled' => ['format' => 'select', 'default' => 'no', 'values' => ['yes', 'no'], 'title' => I18n::_('Enable navigation to page top'), 'description' => I18n::_('Shows navigation button at the bottom when page is scrolled.')],
     ],
 
     'entryLayout' => [
@@ -101,6 +91,16 @@ $settingsDefinition = [
         'group_gallery' => ['format' => false, 'default' => false, 'title' => I18n::_('Image gallery appearance:')],
         'gallerySlideshowAutoRewind' => ['format' => 'select', 'default' => 'no', 'values' => ['yes', 'no'], 'title' => I18n::_('Slideshow loop mode')],
         'gallerySlideNumberVisibilityDefault' => ['format' => 'select', 'default' => 'yes', 'values' => ['yes', 'no'], 'title' => I18n::_('Show slideshow image numbers'), 'description' => I18n::_('Set the default state of image number visibility in slideshow galleries.')],
+    ],
+
+    'media' => [
+        '_' => ['title' => I18n::_('Media')],
+        'imagesSmallWidth' => ['format' => 'text', 'default' => $options['images']['small_width'], 'css_units' => false, 'title' => I18n::_('Small image width'), 'description' => I18n::_('Maximum size of a small image (visible if \'Small images\' are switched on in the gallery editor). These settings don\'t affect original image.')],
+        'imagesSmallHeight' => ['format' => 'text', 'default' => $options['images']['small_height'], 'css_units' => false, 'title' => I18n::_('Small image height'), 'description' => ''],
+        'imagesMediumWidth' => ['format' => 'text', 'default' => $options['images']['medium_width'], 'css_units' => false, 'title' => I18n::_('Medium image width'), 'description' => I18n::_('Maximum size of a medium image (visible if \'Medium images\' are switched on in the gallery editor). These settings don\'t affect original image.')],
+        'imagesMediumHeight' => ['format' => 'text', 'default' => $options['images']['medium_height'], 'css_units' => false, 'title' => I18n::_('Medium image height'), 'description' => ''],
+        'imagesLargeWidth' => ['format' => 'text', 'default' => $options['images']['large_width'], 'css_units' => false, 'title' => I18n::_('Large image width'), 'description' => I18n::_('Maximum size of a large image (visible if \'Large images\' are switched on in the gallery editor). These settings don\'t affect original image.')],
+        'imagesLargeHeight' => ['format' => 'text', 'default' => $options['images']['large_height'], 'css_units' => false, 'title' => I18n::_('Large image height'), 'description' => ''],
     ],
 
     'socialMediaLinks' => [
