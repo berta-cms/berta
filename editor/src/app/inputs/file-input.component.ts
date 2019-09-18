71857b3e7efd93bdb2878490a3d2ad773f308f06
Uldis Rudzitis <uldis.rudzitis@gmail.com>
2019-09-18 14:15:24 +0300

    `Clear` icon red on hover. Improved accessibility for file input component.

--- editor/src/app/inputs/file-input.component.ts
+++ editor/src/app/inputs/file-input.component.ts
@@ -15,13 +15,24 @@ import { SettingModel } from '../shared/interfaces';
                  [disabled]="disabled"
                  type="file"
                  (change)="onChange(fileInput)">
-          <svg *ngIf="!value" width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg *ngIf="!value"
+               role="button"
+               tabindex="0"
+               aria-label="add"
+               class="add"
+               width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <title>add</title>
             <path fill-rule="evenodd" clip-rule="evenodd" d="M6 12C9.31371 12 12 9.31371 12 6C12 2.68629 9.31371 0 6 0C2.68629 0 0 2.68629 0 6C0 9.31371 2.68629 12 6 12Z" fill="#9b9b9b" class="icon"/>
             <path d="M6 3.33333V8.66667" stroke="white" stroke-linecap="square"/><path d="M8.66671 6H3.33337" stroke="white" stroke-linecap="square"/>
           </svg>
           <svg *ngIf="value"
                (click)="removeFile($event, fileInput)"
+               role="button"
+               tabindex="0"
+               aria-label="clear"
+               class="icon clear"
                width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <title>clear</title>
             <path d="M9.8284 9.19243L4.17155 3.53557" stroke="#9B9B9B" stroke-linecap="square" class="icon-remove"/>
             <path d="M9.82844 3.53552L4.17159 9.19237" stroke="#9B9B9B" stroke-linecap="square" class="icon-remove"/>
           </svg>
