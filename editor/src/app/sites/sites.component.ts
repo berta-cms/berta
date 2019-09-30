2031c37da4968c52abae1e6bc2b3abdcc1378e49
Luc Van De Velde <luc.vandevelde@viacesi.fr>
2019-09-30 12:39:36 +0200

    add  the switch to the newly created site

--- editor/src/app/sites/sites.component.ts
+++ editor/src/app/sites/sites.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Store, Select } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { SiteStateModel } from './sites-state/site-state.model';
 import { UpdateInputFocus } from '../app-state/app.actions';
 import { CreateSiteAction, ReOrderSitesAction } from './sites-state/sites.actions';
+import { take, map, switchMap, pairwise } from 'rxjs/operators';
+import { SitesState } from './sites-state/sites.state';
 
 @Component({
   selector: 'berta-sites',
@@ -22,8 +25,9 @@ export class SitesComponent implements OnInit {
   @Select('sites') public sites$: Observable<SiteStateModel[]>;
   sitesList: SiteStateModel[];
 
-
-  constructor(private store: Store) { }
+  constructor(private store: Store,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.sites$.subscribe(sites => {
@@ -36,7 +40,29 @@ export class SitesComponent implements OnInit {
   }
 
   createSite() {
-    this.store.dispatch(CreateSiteAction);
+    let newName;
+    this.store.select(SitesState).pipe(
+      pairwise(),
+      take(2),
+      switchMap(state => {
+        return this.route.queryParams.pipe(
+          take(1),
+          map(() => state),
+        )
+      }),
+    ).subscribe((state => {
+      if (state[0].length !== state[1].length) {
+        state[1].forEach(name => {
+          if (!state[0].includes(name['name'])) {
+            newName = name['name'];
+          }
+        });
+      }
+    }));
+
+    this.store.dispatch(CreateSiteAction).subscribe((state => {
+      this.router.navigate([], { queryParams: { site: newName } });
+    }));
   }
 
   onDrop(event: CdkDragDrop<string[]>) {
