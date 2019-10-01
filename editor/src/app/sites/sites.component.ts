b5473fafe4cd1602d6d2b8ef2a1812c2f96958fc
Luc Van De Velde <luc.vandevelde@viacesi.fr>
2019-10-01 12:41:23 +0200

    Simplified site create observable

--- editor/src/app/sites/sites.component.ts
+++ editor/src/app/sites/sites.component.ts
@@ -40,34 +40,22 @@ export class SitesComponent implements OnInit {
   }
 
   createSite() {
-    let newName;
-    const names = [[], []];
     this.store.select(SitesState).pipe(
-      pairwise(),
-      take(2),
-      switchMap(state => {
-        return this.route.queryParams.pipe(
-          take(1),
-          map(() => state),
-        )
+      take(1),
+      map((sites: SiteStateModel[]) => {
+        return sites.map(site => site.name);
       }),
-    ).subscribe((state => {
-      state[0].forEach(state => {
-        names[0].push(state['name'])
-      });
-      state[1].forEach(state => {
-        names[1].push(state['name'])
-      });
-      names[1].forEach(name => {
-        if (!names[0].includes(name)) {
-          newName = name;
-        }
-      });
-    }));
-
-    this.store.dispatch(CreateSiteAction).subscribe((state => {
-      this.router.navigate([], { queryParams: { site: newName } });
-    }));
+      switchMap(siteNames => this.store.dispatch(CreateSiteAction).pipe(
+        map(({sites: sitesState}) => {
+          return sitesState.find(site => siteNames.indexOf(site.name) === -1);
+        })
+      ))
+    ).subscribe(newSite => {
+      if (!newSite) {
+        return;
+      }
+      this.router.navigate([], { queryParams: { site: newSite.name } });
+    });
   }
 
   onDrop(event: CdkDragDrop<string[]>) {
