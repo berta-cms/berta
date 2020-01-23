b2dee590b9be551d8ad81d097e72a2d3c5dc8b00
Uldis Rudzitis <uldis.rudzitis@gmail.com>
2020-01-23 12:50:41 +0200

    List themes

--- editor/src/app/themes/themes.component.ts
+++ editor/src/app/themes/themes.component.ts
@@ -1,18 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngxs/store';
+
+import { AppStateModel } from '../app-state/app-state.interface';
+import { AppState } from '../app-state/app.state';
 
 @Component({
   selector: 'berta-themes',
   template: `
   <div class="themes-container setting-group">
-    [themes list]
+    <div *ngFor="let theme of appState.themes">
+      {{ theme }}
+      [<a href="#" (click)="previewTheme($event, theme)">Preview</a>]
+      [<a href="#" (click)="applyTheme($event, theme)">Apply</a>]
+    </div>
   </div>
   `
 })
 export class ThemesComponent implements OnInit {
-  constructor() {
+  appState: AppStateModel;
+
+  constructor(
+    private store: Store) {
   }
 
   ngOnInit() {
+    this.store.select(AppState).subscribe((state: AppStateModel) => {
+      this.appState = state;
+    });
+  }
 
+  previewTheme(event, theme) {
+    event.preventDefault();
+    console.log('previewTheme:', theme);
+  }
+
+  applyTheme(event, theme) {
+    event.preventDefault();
+    console.log('applyTheme:', theme);
   }
 }
