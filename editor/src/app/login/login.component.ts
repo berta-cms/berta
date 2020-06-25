068a194de4392f64d377f9029ec3161ad0e2ca19
Uldis Rudzitis <uldis.rudzitis@gmail.com>
2020-06-25 16:44:45 +0300

    Temporary disable FB login

--- editor/src/app/login/login.component.ts
+++ editor/src/app/login/login.component.ts
@@ -26,6 +26,10 @@ import { PopupService } from '../popup/popup.service';
 
     <ng-template #logInForm>
       <div *ngIf="appState.isBertaHosting" class="form-group social-login">
+        <div class="notification">
+          Facebook login temporary disabled. <br>
+          Use <a href="http://hosting.local.berta.me/forgot-password-">password reset</a> and login using email address.
+        </div>
         <a href="{{ appState.loginUrl }}?remote_redirect={{ appState.authenticateUrl }}&amp;provider=facebook" class="button facebook">
         <bt-icon-facebook></bt-icon-facebook>
         <p>Log in with Facebook</p></a>
