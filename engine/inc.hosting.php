4fa5eed3f730dd157d6fc2074d7939940af1cd26
Uldis Rudzitis <uldis.rudzitis@gmail.com>
2020-09-28 17:08:03 +0300

    Fix: add email from address setting

--- engine/inc.hosting.php
+++ engine/inc.hosting.php
@@ -8,6 +8,7 @@ $options['HOSTING_PROFILE'] = isset($hostingConfig['login']) ? $hostingConfig['l
 $options['FORGOTPASSWORD_LINK'] = isset($hostingConfig['forgotPassword']) ? $hostingConfig['forgotPassword'] : 'http://support.berta.me/kb/login-name-and-password/forgot-my-password-for-self-hosted-berta';
 $options['INTERCOM_APP_ID'] = isset($hostingConfig['intercomAppId']) ? $hostingConfig['intercomAppId'] : false;
 $options['INTERCOM_SECRET_KEY'] = isset($hostingConfig['intercomSecretKey']) ? $hostingConfig['intercomSecretKey'] : false;
+$options['EMAIL_FROM_ADDRESS'] = isset($hostingConfig['emailFromAddress']) ? $hostingConfig['emailFromAddress'] : false;
 
 //individual hosting config file for berta
 if (file_exists($ENGINE_ROOT_PATH . 'hosting_config')) {
