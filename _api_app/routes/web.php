979a339a30fca4098a4590480b5e0a6906bb4adf
Uldis Rudzitis <uldis.rudzitis@gmail.com>
2021-10-15 13:05:23 +0300

    Add routes

--- _api_app/routes/web.php
+++ _api_app/routes/web.php
@@ -23,10 +23,74 @@ $router->group(['middleware' => 'setup'], function () use ($router) {
     $router->get('v1/sentry-dsn', ['uses' => 'StateController@getSentryDSN']);
 });
 
-$router->group(['prefix' => 'v1','namespace' => 'App\Http\Controllers', 'middleware' => ['setup', 'auth']], function () use ($router) {
+$router->group(['prefix' => 'v1', 'middleware' => ['setup', 'auth:jwt_token']], function () use ($router) {
 
     $router->patch('user/changepassword', 'AuthController@changePassword');
 
     $router->get('state[/{site}]', 'StateController@get');
     $router->get('locale-settings', ['as'=>'locale_settings', 'prefix'=>'locale_settings', 'uses' => 'StateController@getLocaleSettings']);
+});
+
+$router->group(['prefix' => 'v1','namespace' => 'App\Sites', 'middleware' => ['setup', 'auth:jwt_token']], function () use ($router) {
+    $router->post('sites', ['as' => 'sites', 'uses' => 'SitesController@create']);
+    $router->patch('sites', 'SitesController@update');
+    $router->put('sites', 'SitesController@order');
+    $router->delete('sites', 'SitesController@delete');
+
+    $router->post('sites/theme-preview', ['as' => 'site_theme_preview', 'uses' => 'SitesController@themePreview']);
+    $router->put('sites/theme-apply', ['as' => 'site_theme_apply', 'uses' => 'SitesController@themeApply']);
+
+    $router->get('sites/render-menu[/{site}]', 'SitesController@renderMenu');
+    $router->get('sites/render-header[/{site}]', 'SitesController@renderHeader');
+    $router->get('sites/render-banners[/{site}]', 'SitesController@renderBanners');
+    $router->get('sites/render-social-media-links[/{site}]', 'SitesController@renderSocialMediaLinks');
+
+    $router->patch('sites/settings', ['as' => 'site_settings', 'uses' => 'Settings\SiteSettingsController@update']);
+    $router->post('sites/settings/upload', ['as' => 'site_settings_upload', 'uses' => 'Settings\SiteSettingsController@upload']);
+    $router->post('sites/settings', 'Settings\SiteSettingsController@createChildren');
+    $router->delete('sites/settings', 'Settings\SiteSettingsController@deleteChildren');
+
+    $router->patch('sites/template-settings', ['as' => 'site_template_settings', 'uses' => 'TemplateSettings\SiteTemplateSettingsController@update']);
+    $router->post('sites/template-settings/upload', ['as' => 'site_template_settings_upload', 'uses' => 'TemplateSettings\SiteTemplateSettingsController@upload']);
+});
+
+$router->group(['prefix' => 'v1/sites', 'namespace' => 'App\Sites\Sections', 'middleware' => ['setup', 'auth']], function () use ($router) {
+    $router->post('sections', ['as' => 'site_sections', 'uses' => 'SiteSectionsController@create']);
+    $router->patch('sections', 'SiteSectionsController@update');
+    $router->patch('sections/reset', ['as' => 'site_sections_reset', 'uses' => 'SiteSectionsController@reset']);
+    $router->put('sections', 'SiteSectionsController@order');
+    $router->delete('sections', 'SiteSectionsController@delete');
+
+    $router->get('sections/render-head[/{site}]', 'SiteSectionsController@renderHead');
+    $router->get('sections/render-menu[/{site}]', 'SiteSectionsController@renderMenu');
+    $router->get('sections/render-sitemap[/{siteSlug}]', 'SiteSectionsController@renderSitemap');
+    $router->get('sections/render-background-gallery[/{siteSlug}]', 'SiteSectionsController@renderBackgroundGallery');
+    $router->get('sections/render-background-gallery-editor[/{siteSlug}]', 'SiteSectionsController@renderBackgroundGalleryEditor');
+    $router->get('sections/render-grid-view[/{siteSlug}]', 'SiteSectionsController@renderGridView');
+    $router->get('sections/render-additional-text[/{site}]', 'SiteSectionsController@renderAdditionalText');
+    $router->get('sections/render-additional-footer-text[/{site}]', 'SiteSectionsController@renderAdditionalFooterText');
+    $router->get('sections/render-footer[/{site}]', 'SiteSectionsController@renderFooter');
+    $router->get('sections/render-template[/{siteSlug}]', 'SiteSectionsController@renderTemplate');
+
+    $router->put('sections/backgrounds', ['as' => 'site_section_backgrounds', 'uses' => 'SiteSectionsController@backgroundGalleryOrder']);
+    $router->post('sections/backgrounds', 'SiteSectionsController@backgroundGalleryUpload');
+    $router->delete('sections/backgrounds', 'SiteSectionsController@backgroundGalleryDelete');
+
+    $router->put('sections/tags', ['as' => 'section_tags', 'uses' => 'Tags\SectionTagsController@order']);
+});
+
+$router->group(['prefix' => 'v1/sites/sections', 'namespace' => 'App\Sites\Sections\Entries', 'middleware' => ['setup', 'auth']], function () use ($router) {
+    $router->patch('entries', ['as' => 'section_entries', 'uses' => 'SectionEntriesController@update']);
+    $router->post('entries', 'SectionEntriesController@create');
+    $router->put('entries', 'SectionEntriesController@order');
+    $router->delete('entries', 'SectionEntriesController@delete');
+    $router->patch('entries/move', ['as' => 'section_entries_move', 'uses' => 'SectionEntriesController@move']);
+    $router->put('entries/galleries', ['as' => 'entry_gallery', 'uses' => 'SectionEntriesController@galleryOrder']);
+    $router->post('entries/galleries', 'SectionEntriesController@galleryUpload');
+    $router->patch('entries/galleries', 'SectionEntriesController@galleryCrop');
+    $router->delete('entries/galleries', 'SectionEntriesController@galleryDelete');
+    $router->get('entries/render/{site}/{section}[/{id}]', 'SectionEntriesController@renderEntries');
+    $router->get('entries/render-gallery-editor/{site}/{section}/{id}', 'SectionEntriesController@renderEntryGalleryEditor');
+    $router->get('entries/render-mashup[/{site}]', 'SectionEntriesController@renderMashupEntries');
+    $router->get('entries/render-portfolio-thumbnails/{site}/{section}', 'SectionEntriesController@renderPortfolioThumbnails');
 });
