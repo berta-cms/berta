935452849b373a091faeb4bebc5b80152157ac73
Uldis Rudzitis <uldis.rudzitis@gmail.com>
2023-02-28 12:25:52 +0200

    Fix types

--- editor/src/app/sites/sections/section.component.ts
+++ editor/src/app/sites/sections/section.component.ts
@@ -6,8 +6,14 @@ import { Store } from '@ngxs/store';
 import { Animations } from '../../shared/animations';
 import { PopupService } from 'src/app/popup/popup.service';
 import { SiteSectionStateModel } from './sections-state/site-sections-state.model';
-import { SiteTemplateSectionTypesModel, TemplateTranslationsModel } from '../template-settings/site-templates.interface';
-import { DeleteSiteSectionAction, CloneSectionAction } from './sections-state/site-sections.actions';
+import {
+  SiteTemplateSectionTypesModel,
+  TemplateTranslationsModel,
+} from '../template-settings/site-templates.interface';
+import {
+  DeleteSiteSectionAction,
+  CloneSectionAction,
+} from './sections-state/site-sections.actions';
 import { AppState } from 'src/app/app-state/app.state';
 
 @Component({
@@ -15,50 +21,76 @@ import { AppState } from 'src/app/app-state/app.state';
   template: `
     <div class="setting-group" [class.is-expanded]="isExpanded">
       <h3>
-        <berta-inline-text-input [value]="section.title"
-                                 [class.clickable-text]="isClickableText"
-                                 (inputFocus)="updateComponentFocus($event)"
-                                 (update)="updateTextField('title', $event)"
-                                 (textClick)="switchSection(section.name)"></berta-inline-text-input>
+        <berta-inline-text-input
+          [value]="section.title"
+          [class.clickable-text]="isClickableText"
+          (inputFocus)="updateComponentFocus($event)"
+          (update)="updateTextField('title', $event)"
+          (textClick)="switchSection(section.name)"
+        ></berta-inline-text-input>
 
         <div class="expand"></div>
-        <button [attr.title]="(section['@attributes'].published > 0 ? 'Unpublish': 'Publish')"
-                (click)="updateField({'@attributes': {published: (section['@attributes'].published > 0 ? '0' : '1')}})">
-          <berta-icon-publish [published]="(section['@attributes'].published > 0)"></berta-icon-publish>
+        <button
+          [attr.title]="
+            section['@attributes'].published === '1' ? 'Unpublish' : 'Publish'
+          "
+          (click)="
+            updateField({
+              '@attributes': {
+                published: section['@attributes'].published === '1' ? '0' : '1'
+              }
+            })
+          "
+        >
+          <berta-icon-publish
+            [published]="section['@attributes'].published === '1'"
+          ></berta-icon-publish>
         </button>
-        <button title="copy"
-                (click)="cloneSection()">
+        <button title="copy" (click)="cloneSection()">
           <bt-icon-clone></bt-icon-clone>
         </button>
-        <button title="delete"
-                class="delete"
-                (click)="deleteSection()">
+        <button title="delete" class="delete" (click)="deleteSection()">
           <bt-icon-delete></bt-icon-delete>
         </button>
-        <button title="settings"
-                [class.active]="isExpanded"
-                [routerLink]="['/sections', (isExpanded ? '' : section.name)]"
-                queryParamsHandling="preserve">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" version="1.1" viewBox="0 0 16 16">
-            <path d="m10.7 8q0-1.1-0.8-1.9-0.8-0.8-1.9-0.8t-1.9 0.8q-0.8 0.8-0.8 1.9t0.8 1.9q0.8 0.8 1.9 0.8t1.9-0.8q0.8-0.8 0.8-1.9zm5.3-1.1v2.3q0 0.1-0.1 0.2t-0.2 0.1l-1.9 0.3q-0.2 0.6-0.4 0.9 0.4 0.5 1.1 1.4 0.1 0.1 0.1 0.3t-0.1 0.2q-0.3 0.4-1 1.1t-1 0.7q-0.1 0-0.3-0.1l-1.4-1.1q-0.5 0.2-0.9 0.4-0.2 1.4-0.3 1.9-0.1 0.3-0.4 0.3h-2.3q-0.1 0-0.3-0.1-0.1-0.1-0.1-0.2l-0.3-1.9q-0.5-0.2-0.9-0.4l-1.5 1.1q-0.1 0.1-0.3 0.1-0.1 0-0.3-0.1-1.3-1.2-1.7-1.7-0.1-0.1-0.1-0.2 0-0.1 0.1-0.2 0.2-0.2 0.5-0.7 0.4-0.5 0.6-0.7-0.3-0.5-0.4-1l-1.9-0.3q-0.1 0-0.2-0.1-0.1-0.1-0.1-0.2v-2.3q0-0.1 0.1-0.2 0.1-0.1 0.2-0.1l1.9-0.3q0.1-0.5 0.4-1-0.4-0.6-1.1-1.4-0.1-0.1-0.1-0.2 0-0.1 0.1-0.2 0.3-0.4 1-1.1 0.8-0.7 1-0.7 0.1 0 0.3 0.1l1.4 1.1q0.5-0.2 0.9-0.4 0.2-1.4 0.3-1.9 0.1-0.3 0.4-0.3h2.3q0.1 0 0.3 0.1 0.1 0.1 0.1 0.2l0.3 1.9q0.5 0.2 0.9 0.4l1.5-1.1q0.1-0.1 0.3-0.1 0.1 0 0.3 0.1 1.3 1.2 1.7 1.8 0.1 0.1 0.1 0.2 0 0.1-0.1 0.2-0.2 0.2-0.5 0.7t-0.6 0.7q0.3 0.5 0.4 1l1.9 0.3q0.1 0 0.2 0.1 0.1 0.1 0.1 0.2z" stroke-width="0"/>
+        <button
+          title="settings"
+          [class.active]="isExpanded"
+          [routerLink]="['/sections', isExpanded ? '' : section.name]"
+          queryParamsHandling="preserve"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            version="1.1"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="m10.7 8q0-1.1-0.8-1.9-0.8-0.8-1.9-0.8t-1.9 0.8q-0.8 0.8-0.8 1.9t0.8 1.9q0.8 0.8 1.9 0.8t1.9-0.8q0.8-0.8 0.8-1.9zm5.3-1.1v2.3q0 0.1-0.1 0.2t-0.2 0.1l-1.9 0.3q-0.2 0.6-0.4 0.9 0.4 0.5 1.1 1.4 0.1 0.1 0.1 0.3t-0.1 0.2q-0.3 0.4-1 1.1t-1 0.7q-0.1 0-0.3-0.1l-1.4-1.1q-0.5 0.2-0.9 0.4-0.2 1.4-0.3 1.9-0.1 0.3-0.4 0.3h-2.3q-0.1 0-0.3-0.1-0.1-0.1-0.1-0.2l-0.3-1.9q-0.5-0.2-0.9-0.4l-1.5 1.1q-0.1 0.1-0.3 0.1-0.1 0-0.3-0.1-1.3-1.2-1.7-1.7-0.1-0.1-0.1-0.2 0-0.1 0.1-0.2 0.2-0.2 0.5-0.7 0.4-0.5 0.6-0.7-0.3-0.5-0.4-1l-1.9-0.3q-0.1 0-0.2-0.1-0.1-0.1-0.1-0.2v-2.3q0-0.1 0.1-0.2 0.1-0.1 0.2-0.1l1.9-0.3q0.1-0.5 0.4-1-0.4-0.6-1.1-1.4-0.1-0.1-0.1-0.2 0-0.1 0.1-0.2 0.3-0.4 1-1.1 0.8-0.7 1-0.7 0.1 0 0.3 0.1l1.4 1.1q0.5-0.2 0.9-0.4 0.2-1.4 0.3-1.9 0.1-0.3 0.4-0.3h2.3q0.1 0 0.3 0.1 0.1 0.1 0.1 0.2l0.3 1.9q0.5 0.2 0.9 0.4l1.5-1.1q0.1-0.1 0.3-0.1 0.1 0 0.3 0.1 1.3 1.2 1.7 1.8 0.1 0.1 0.1 0.2 0 0.1-0.1 0.2-0.2 0.2-0.5 0.7t-0.6 0.7q0.3 0.5 0.4 1l1.9 0.3q0.1 0 0.2 0.1 0.1 0.1 0.1 0.2z"
+              stroke-width="0"
+            />
           </svg>
         </button>
       </h3>
       <div class="settings" [@isExpanded]="isExpanded">
         <div class="setting">
-          <berta-select-input [label]="translations.sectionTypes?.type"
-                              [tip]="translations.sectionTypes?.type_tip"
-                              [value]="section['@attributes'].type"
-                              [values]="templateSectionTypes"
-                              (inputFocus)="updateComponentFocus($event)"
-                              (update)="updateField({'@attributes': {type: $event}})"></berta-select-input>
+          <berta-select-input
+            [label]="translations.sectionTypes?.type"
+            [tip]="translations.sectionTypes?.type_tip"
+            [value]="section['@attributes'].type"
+            [values]="templateSectionTypes"
+            (inputFocus)="updateComponentFocus($event)"
+            (update)="updateField({ '@attributes': { type: $event } })"
+          ></berta-select-input>
         </div>
 
         <div *ngIf="params.length > 0" class="section-params">
-          <berta-setting *ngFor="let param of params"
-                        [setting]="param.setting"
-                        [config]="param.config"
-                        (update)="updateSectionParams($event)"></berta-setting>
+          <berta-setting
+            *ngFor="let param of params"
+            [setting]="param.setting"
+            [config]="param.config"
+            (update)="updateSectionParams($event)"
+          ></berta-setting>
         </div>
 
         <div class="setting">
@@ -66,62 +98,76 @@ import { AppState } from 'src/app/app-state/app.state';
         </div>
 
         <div class="setting">
-          <berta-long-text-input [label]="'Title'"
-                                 [value]="section.seoTitle"
-                                 (inputFocus)="updateComponentFocus($event)"
-                                 (update)="updateTextField('seoTitle', $event)"></berta-long-text-input>
+          <berta-long-text-input
+            [label]="'Title'"
+            [value]="section.seoTitle"
+            (inputFocus)="updateComponentFocus($event)"
+            (update)="updateTextField('seoTitle', $event)"
+          ></berta-long-text-input>
         </div>
 
         <div class="setting">
-          <berta-long-text-input [label]="'Keywords'"
-                                 [value]="section.seoKeywords"
-                                 (inputFocus)="updateComponentFocus($event)"
-                                 (update)="updateTextField('seoKeywords', $event)"></berta-long-text-input>
+          <berta-long-text-input
+            [label]="'Keywords'"
+            [value]="section.seoKeywords"
+            (inputFocus)="updateComponentFocus($event)"
+            (update)="updateTextField('seoKeywords', $event)"
+          ></berta-long-text-input>
         </div>
 
         <div class="setting">
-          <berta-long-text-input [label]="'Description'"
-                                 [value]="section.seoDescription"
-                                 (inputFocus)="updateComponentFocus($event)"
-                                 (update)="updateTextField('seoDescription', $event)"></berta-long-text-input>
+          <berta-long-text-input
+            [label]="'Description'"
+            [value]="section.seoDescription"
+            (inputFocus)="updateComponentFocus($event)"
+            (update)="updateTextField('seoDescription', $event)"
+          ></berta-long-text-input>
         </div>
       </div>
     </div>
   `,
-  styles: [`
-    :host {
-      display: block;
-    }
+  styles: [
+    `
+      :host {
+        display: block;
+      }
 
-    .expand,
-    h3 input[type=text] {
-      flex-grow: 1;
-    }
-  `],
-  animations: [
-    Animations.slideToggle
-  ]
+      .expand,
+      h3 input[type='text'] {
+        flex-grow: 1;
+      }
+    `,
+  ],
+  animations: [Animations.slideToggle],
 })
 export class SectionComponent implements OnInit {
   @Input('section') section: SiteSectionStateModel;
   @Input('isExpanded') isExpanded: boolean;
-  @Input('templateSectionTypes') templateSectionTypes: SiteTemplateSectionTypesModel;
+  @Input('templateSectionTypes')
+  templateSectionTypes: SiteTemplateSectionTypesModel;
   @Input('translations') translations: TemplateTranslationsModel;
   @Input('params') params: any[] = [];
 
   @Output() inputFocus = new EventEmitter();
-  @Output('update') update = new EventEmitter<{section: string|number, data: {[k: string]: any}}>();
+  @Output('update') update = new EventEmitter<{
+    section: string | number;
+    data: { [k: string]: any };
+  }>();
 
   isClickableText: boolean;
 
-
-  constructor(private store: Store,
+  constructor(
+    private store: Store,
     private router: Router,
     private route: ActivatedRoute,
-    private popupService: PopupService) { }
+    private popupService: PopupService
+  ) {}
 
   ngOnInit() {
-    this.isClickableText = !(this.section['@attributes'].type && this.section['@attributes'].type === 'external_link');
+    this.isClickableText = !(
+      this.section['@attributes'].type &&
+      this.section['@attributes'].type === 'external_link'
+    );
   }
 
   switchSection(sectionName) {
@@ -129,7 +175,10 @@ export class SectionComponent implements OnInit {
       return;
     }
     const siteName = this.store.selectSnapshot(AppState.getSite);
-    this.router.navigate([], {queryParams: {site: siteName ? siteName : null, section: sectionName}, queryParamsHandling: 'merge'});
+    this.router.navigate([], {
+      queryParams: { site: siteName ? siteName : null, section: sectionName },
+      queryParamsHandling: 'merge',
+    });
   }
 
   updateComponentFocus(isFocused) {
@@ -146,13 +195,13 @@ export class SectionComponent implements OnInit {
   updateField(data) {
     this.update.emit({
       section: this.section.order,
-      data
+      data,
     });
   }
 
   updateSectionParams(updateEvent) {
     this.updateField({
-      [updateEvent.field]: updateEvent.value
+      [updateEvent.field]: updateEvent.value,
     });
   }
 
@@ -170,41 +219,50 @@ export class SectionComponent implements OnInit {
           type: 'primary',
           label: 'OK',
           callback: (popupService) => {
-            this.store.dispatch(new DeleteSiteSectionAction(this.section)).pipe(
-              switchMap(() => {
-                return combineLatest(
-                  this.route.queryParams,
-                  this.route.paramMap
-                );
-              }),
-              take(1),
-              map(([query, params]) => {
-                const obj = {};
+            this.store
+              .dispatch(new DeleteSiteSectionAction(this.section))
+              .pipe(
+                switchMap(() => {
+                  return combineLatest(
+                    this.route.queryParams,
+                    this.route.paramMap
+                  );
+                }),
+                take(1),
+                map(([query, params]) => {
+                  const obj = {};
 
-                if (query.section && query.section === this.section.name) {
-                  obj['query'] = {section: null};
-                }
+                  if (query.section && query.section === this.section.name) {
+                    obj['query'] = { section: null };
+                  }
 
-                if (params['params']['section'] && params['params']['section'] === this.section.name) {
-                  obj['params'] = ['/sections'];
-                }
+                  if (
+                    params['params']['section'] &&
+                    params['params']['section'] === this.section.name
+                  ) {
+                    obj['params'] = ['/sections'];
+                  }
 
-                return obj;
-              }),
-              filter(obj => !!Object.keys(obj).length)
-            ).subscribe(obj => {
-              const route = 'params' in obj ? obj['params'] : [];
-              const qParams = 'query' in obj ? obj['query'] : {};
+                  return obj;
+                }),
+                filter((obj) => !!Object.keys(obj).length)
+              )
+              .subscribe((obj) => {
+                const route = 'params' in obj ? obj['params'] : [];
+                const qParams = 'query' in obj ? obj['query'] : {};
 
-              this.router.navigate(route, {queryParams: qParams, queryParamsHandling: 'merge'});
-            });
+                this.router.navigate(route, {
+                  queryParams: qParams,
+                  queryParamsHandling: 'merge',
+                });
+              });
 
             popupService.closePopup();
-          }
+          },
         },
         {
-          label: 'Cancel'
-        }
+          label: 'Cancel',
+        },
       ],
     });
   }
