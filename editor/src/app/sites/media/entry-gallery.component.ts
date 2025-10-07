df79986ca4e87a871e1183dab7f7e8633a3e97e1
Uldis Rudzitis <uldis.rudzitis@gmail.com>
2025-10-07 18:30:21 +0300

    Move dev dependency, remove unused imports

--- editor/src/app/sites/media/entry-gallery.component.ts
+++ editor/src/app/sites/media/entry-gallery.component.ts
@@ -1,41 +1,33 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Store } from '@ngxs/store';
-import {
-  SectionEntry,
-  SectionEntryGalleryFile,
-} from '../sections/entries/entries-state/section-entries-state.model';
+import { SectionEntry } from '../sections/entries/entries-state/section-entries-state.model';
 import { AppState } from '../../../app/app-state/app.state';
 import { SiteStateModel } from '../sites-state/site-state.model';
 
 @Component({
-    selector: 'berta-entry-gallery',
-    template: `
+  selector: 'berta-entry-gallery',
+  template: `
     <div class="entry-gallery">
       <div class="header">
         entry #{{ entry.id }}
         @if (entry && entry.tags && entry.tags.tag.length > 0) {
-          <span
-            >({{ entry.tags.tag.join(', ') }})</span
-            >
-          }
-        </div>
-        <div class="entry-gallery-items">
-          @for (file of entry.mediaCacheData.file; track file) {
-            <div
-              class="entry-gallery-item"
-              >
-              @if (file['@attributes'].type === 'image') {
-                <div class="media image">
-                  <img
+        <span>({{ entry.tags.tag.join(', ') }})</span>
+        }
+      </div>
+      <div class="entry-gallery-items">
+        @for (file of entry.mediaCacheData.file; track file) {
+        <div class="entry-gallery-item">
+          @if (file['@attributes'].type === 'image') {
+          <div class="media image">
+            <img
               src="{{ currentSite.mediaUrl }}/{{
                 entry.mediafolder
               }}/_smallthumb_{{ file['@attributes'].src }}"
-                    />
-                </div>
-              }
-              @if (file['@attributes'].type === 'video') {
-                <div class="media video">
-                  <video
+            />
+          </div>
+          } @if (file['@attributes'].type === 'video') {
+          <div class="media video">
+            <video
               [attr.poster]="
                 file['@attributes'].poster_frame
                   ? currentSite.mediaUrl +
@@ -45,26 +37,26 @@ import { SiteStateModel } from '../sites-state/site-state.model';
                     file['@attributes'].poster_frame
                   : null
               "
-                    >
-                    <source
+            >
+              <source
                 src="{{ currentSite.mediaUrl }}/{{ entry.mediafolder }}/{{
                   file['@attributes'].src
                 }}"
-                      type="video/mp4"
-                      />
-                  </video>
-                </div>
-              }
-            </div>
+                type="video/mp4"
+              />
+            </video>
+          </div>
           }
         </div>
-        <berta-route-button
-          [label]="'Edit gallery'"
-          [route]="'/media/gallery/' + entry.sectionName + '/' + entry.id"
-        ></berta-route-button>
+        }
       </div>
-    `,
-    standalone: false
+      <berta-route-button
+        [label]="'Edit gallery'"
+        [route]="'/media/gallery/' + entry.sectionName + '/' + entry.id"
+      ></berta-route-button>
+    </div>
+  `,
+  standalone: false,
 })
 export class EntryGalleryComponent {
   @Input('entry') entry: SectionEntry;
